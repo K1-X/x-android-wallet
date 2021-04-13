ff3ff2b992b3b39395d244888e821ed393892e62
madisonpill <madisonpill@hotmail.com>
2021-04-13 19:20:33 +0800

    convert util

---
+++ appcore/src/main/java/bd/com/appcore/util/ConvertUtils.java
@@ -0,0 +1,391 @@
+package bd.com.appcore.util;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
+/**
+ * <pre>
+ *     author: Blankj
+ *     blog  : http://blankj.com
+ *     time  : 2016/8/13
+ *     desc  : 
+ * </pre>
+ */
+public class ConvertUtils {
+
+    private ConvertUtils() {
+        throw new UnsupportedOperationException("u can't instantiate me...");
+    }
+
+    private static final char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
+    /**
+     * byteArrhexString
+     * <p>：</p>
+     * bytes2HexString(new byte[] { 0, (byte) 0xa8 }) returns 00A8
+     *
+     * @param bytes 
+     * @return 16
+     */
+    public static String bytes2HexString(byte[] bytes) {
+        if (bytes == null) return null;
+        int len = bytes.length;
+        if (len <= 0) return null;
+        char[] ret = new char[len << 1];
+        for (int i = 0, j = 0; i < len; i++) {
+            ret[j++] = hexDigits[bytes[i] >>> 4 & 0x0f];
+            ret[j++] = hexDigits[bytes[i] & 0x0f];
+        }
+        return new String(ret);
+    }
+
+
+    /**
+     * hexCharint
+     *
+     * @param hexChar hex
+     * @return 0..15
+     */
+    private static int hex2Dec(char hexChar) {
+        if (hexChar >= '0' && hexChar <= '9') {
+            return hexChar - '0';
+        } else if (hexChar >= 'A' && hexChar <= 'F') {
+            return hexChar - 'A' + 10;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    /**
+     * charArrbyteArr
+     *
+     * @param chars 
+     * @return 
+     */
+    public static byte[] chars2Bytes(char[] chars) {
+        if (chars == null || chars.length <= 0) return null;
+        int len = chars.length;
+        byte[] bytes = new byte[len];
+        for (int i = 0; i < len; i++) {
+            bytes[i] = (byte) (chars[i]);
+        }
+        return bytes;
+    }
+
+    /**
+     * byteArrcharArr
+     *
+     * @param bytes 
+     * @return 
+     */
+    public static char[] bytes2Chars(byte[] bytes) {
+        if (bytes == null) return null;
+        int len = bytes.length;
+        if (len <= 0) return null;
+        char[] chars = new char[len];
+        for (int i = 0; i < len; i++) {
+            chars[i] = (char) (bytes[i] & 0xff);
+        }
+        return chars;
+    }
+
+
+    /**
+     * 
+     *
+     * @param millis    
+     *                  <p>0，null</p>
+     * @param precision 
+     *                  <p>precision = 0，null</p>
+     *                  <p>precision = 1，</p>
+     *                  <p>precision = 2，</p>
+     *                  <p>precision = 3，、</p>
+     *                  <p>precision = 4，、、</p>
+     *                  <p>precision >= 5，、、、</p>
+     * @return 
+     */
+    @SuppressLint("DefaultLocale")
+    public static String millis2FitTimeSpan(long millis, int precision) {
+        if (millis <= 0 || precision <= 0) return null;
+        StringBuilder sb = new StringBuilder();
+        String[] units = {"", "", "", "", ""};
+        int[] unitLen = {86400000, 3600000, 60000, 1000, 1};
+        precision = Math.min(precision, 5);
+        for (int i = 0; i < precision; i++) {
+            if (millis >= unitLen[i]) {
+                long mode = millis / unitLen[i];
+                millis -= mode * unitLen[i];
+                sb.append(mode).append(units[i]);
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * bytesbits
+     *
+     * @param bytes 
+     * @return bits
+     */
+    public static String bytes2Bits(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte aByte : bytes) {
+            for (int j = 7; j >= 0; --j) {
+                sb.append(((aByte >> j) & 0x01) == 0 ? '0' : '1');
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * bitsbytes
+     *
+     * @param bits 
+     * @return bytes
+     */
+    public static byte[] bits2Bytes(String bits) {
+        int lenMod = bits.length() % 8;
+        int byteLen = bits.length() / 8;
+        // 80
+        if (lenMod != 0) {
+            for (int i = lenMod; i < 8; i++) {
+                bits = "0" + bits;
+            }
+            byteLen++;
+        }
+        byte[] bytes = new byte[byteLen];
+        for (int i = 0; i < byteLen; ++i) {
+            for (int j = 0; j < 8; ++j) {
+                bytes[i] <<= 1;
+                bytes[i] |= bits.charAt(i * 8 + j) - '0';
+            }
+        }
+        return bytes;
+    }
+
+
+    /**
+     * outputStreaminputStream
+     *
+     * @param out 
+     * @return inputStream
+     */
+    public ByteArrayInputStream output2InputStream(OutputStream out) {
+        if (out == null) return null;
+        return new ByteArrayInputStream(((ByteArrayOutputStream) out).toByteArray());
+    }
+
+
+    /**
+     * byteArrinputStream
+     *
+     * @param bytes 
+     * @return 
+     */
+    public static InputStream bytes2InputStream(byte[] bytes) {
+        if (bytes == null || bytes.length <= 0) return null;
+        return new ByteArrayInputStream(bytes);
+    }
+
+    /**
+     * outputStreambyteArr
+     *
+     * @param out 
+     * @return 
+     */
+    public static byte[] outputStream2Bytes(OutputStream out) {
+        if (out == null) return null;
+        return ((ByteArrayOutputStream) out).toByteArray();
+    }
+
+
+    /**
+     * bitmapbyteArr
+     *
+     * @param bitmap bitmap
+     * @param format 
+     * @return 
+     */
+    public static byte[] bitmap2Bytes(Bitmap bitmap, Bitmap.CompressFormat format) {
+        if (bitmap == null) return null;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(format, 100, baos);
+        return baos.toByteArray();
+    }
+
+    /**
+     * byteArrbitmap
+     *
+     * @param bytes 
+     * @return bitmap
+     */
+    public static Bitmap bytes2Bitmap(byte[] bytes) {
+        return (bytes == null || bytes.length == 0) ? null : BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+    }
+
+    /**
+     * drawablebitmap
+     *
+     * @param drawable drawable
+     * @return bitmap
+     */
+    public static Bitmap drawable2Bitmap(Drawable drawable) {
+        return drawable == null ? null : ((BitmapDrawable) drawable).getBitmap();
+    }
+
+    /**
+     * bitmapdrawable
+     *
+     * @param res    resources
+     * @param bitmap bitmap
+     * @return drawable
+     */
+    public static Drawable bitmap2Drawable(Resources res, Bitmap bitmap) {
+        return bitmap == null ? null : new BitmapDrawable(res, bitmap);
+    }
+
+    /**
+     * drawablebyteArr
+     *
+     * @param drawable drawable
+     * @param format   
+     * @return 
+     */
+    public static byte[] drawable2Bytes(Drawable drawable, Bitmap.CompressFormat format) {
+        return drawable == null ? null : bitmap2Bytes(drawable2Bitmap(drawable), format);
+    }
+
+    /**
+     * byteArrdrawable
+     *
+     * @param res   resources
+     * @param bytes 
+     * @return drawable
+     */
+    public static Drawable bytes2Drawable(Resources res, byte[] bytes) {
+        return res == null ? null : bitmap2Drawable(res, bytes2Bitmap(bytes));
+    }
+
+    /**
+     * viewBitmap
+     *
+     * @param view 
+     * @return bitmap
+     */
+    public static Bitmap view2Bitmap(View view) {
+        if (view == null) return null;
+        Bitmap ret = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(ret);
+        Drawable bgDrawable = view.getBackground();
+        if (bgDrawable != null) {
+            bgDrawable.draw(canvas);
+        } else {
+            canvas.drawColor(Color.WHITE);
+        }
+        view.draw(canvas);
+        return ret;
+    }
+
+    /**
+     * dppx
+     *
+     * @param context 
+     * @param dpValue dp
+     * @return px
+     */
+    public static int dp2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * pxdp
+     *
+     * @param context 
+     * @param pxValue px
+     * @return dp
+     */
+    public static int px2dp(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * sppx
+     *
+     * @param context 
+     * @param spValue sp
+     * @return px
+     */
+    public static int sp2px(Context context, float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+    /**
+     * pxsp
+     *
+     * @param context 
+     * @param pxValue px
+     * @return sp
+     */
+    public static int px2sp(Context context, float pxValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+    /**
+     * 
+     *
+     * @param oldDouble
+     * @param scale
+     * @return
+     */
+    public static String significand(String number, int scale) {
+        if (scale < 0) {
+            throw new IllegalArgumentException(
+                    "scale");
+        }
+        /**
+         * RoundingMode：
+         * UP：；
+         * DOWN：；
+         * CEILING： ；
+         * FLOOR：；
+         * HALF_DOWN：，，；
+         * HALF_UP：，，；
+         * HALF_EVEN：，，;(,)
+         * UNNECESSARY：，。
+         */
+        RoundingMode rMode = null;
+        //rMode=RoundingMode.FLOOR;
+        //，FLOOR。
+        if (new BigDecimal(number).compareTo(new BigDecimal("0")) == 1) {
+            rMode = RoundingMode.DOWN;
+        } else {
+            rMode = RoundingMode.UP;
+        }
+        //scale，
+        BigDecimal b = new BigDecimal(number, new MathContext(scale, rMode));
+        return b.toString();
+    }
+}
