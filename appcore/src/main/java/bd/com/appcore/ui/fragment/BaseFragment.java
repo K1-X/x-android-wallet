03712c3fe3f8dbe2ed1c0a2bed9024eac697b9ca
kolionsleep <kolionsleep@hotmail.com>
2021-04-14 09:11:49 +0800

    BaseFragment

---
+++ appcore/src/main/java/bd/com/appcore/ui/fragment/BaseFragment.java
@@ -0,0 +1,20 @@
+package bd.com.appcore.ui.fragment;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Looper;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
+
+
+public abstract class BaseFragment extends Fragment {
+}
