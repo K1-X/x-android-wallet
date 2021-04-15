f5187dbf446fed4ca4176456f4057fb74b568e87
madisonpill <madisonpill@hotmail.com>
2021-04-15 09:22:20 +0800

    add base dialog fragment class

---
+++ appcore/src/main/java/bd/com/appcore/ui/fragment/BaseDialogFragment.java
@@ -0,0 +1,17 @@
+package bd.com.appcore.ui.fragment;
+
+import android.app.Activity;
+
+import java.util.concurrent.TimeUnit;
+
+import bd.com.appcore.R;
+import bd.com.appcore.ui.view.LoadingDialog;
+import bd.com.appcore.ui.view.ToastDialog;
+import io.reactivex.Observable;
+import io.reactivex.functions.Consumer;
+
+
+public abstract class BaseDialogFragment extends BaseFragment{
+    
+
+}
