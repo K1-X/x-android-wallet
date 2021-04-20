e6992e3acacd324b6d62be3b7d3cb913241fbccb
madisonpill <madisonpill@hotmail.com>
2021-04-20 08:58:05 +0800

    onDraw

--- appcore/src/main/java/bd/com/appcore/ui/view/CircleImageView.java
+++ appcore/src/main/java/bd/com/appcore/ui/view/CircleImageView.java
@@ -88,4 +88,14 @@ private static final ScaleType SCALE_TYPE = ScaleType.CENTER_CROP;
             throw new IllegalArgumentException(String.format("ScaleType %s not supported.", scaleType));
         }
     }
+
+     @Override
+    protected void onDraw(Canvas canvas) {
+        if (getDrawable() == null) {
+            return;
+        }
+
+        canvas.drawCircle(getWidth() / 2, getHeight() / 2, mDrawableRadius, mBitmapPaint);
+        canvas.drawCircle(getWidth() / 2, getHeight() / 2, mBorderRadius, mBorderPaint);
+    }
 }
