2197172a59755f638443a3810639e752dedeb9a7
madisonpill <madisonpill@hotmail.com>
2021-04-20 08:56:06 +0800

    add circle image view

---
+++ appcore/src/main/java/bd/com/appcore/ui/view/CircleImageView.java
@@ -0,0 +1,26 @@
+package bd.com.appcore.ui.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+import bd.com.appcore.R;
+
+
+public class CircleImageView extends ImageView {
+
+   
+
+}
