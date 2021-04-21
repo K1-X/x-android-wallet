bfab9dfacb7b84a9d6810db03b03021d426dfa8e
kolionsleep <kolionsleep@hotmail.com>
2021-04-21 08:05:03 +0800

    add helper class for action bar

---
+++ appcore/src/main/java/bd/com/appcore/ui/view/CommonActionBar.java
@@ -0,0 +1,26 @@
+package bd.com.appcore.ui.view;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Build;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.RequiresApi;
+import android.support.annotation.StringRes;
+import android.support.annotation.StyleableRes;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import bd.com.appcore.R;
+
+
+/**
+ * 
+ */
+public class CommonActionBar extends RelativeLayout {
+
+    
+}
