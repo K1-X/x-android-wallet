444a18e248d704fcaec5e7f4616b377496c20fe9
madisonpill <madisonpill@hotmail.com>
2021-04-22 08:55:59 +0800

    add common line text view

---
+++ appcore/src/main/java/bd/com/appcore/ui/view/CommonLineTextView.java
@@ -0,0 +1,21 @@
+package bd.com.appcore.ui.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.annotation.StringRes;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import bd.com.appcore.R;
+
+
+/**
+ * Created by  on 2017/8/17.
+ */
+
+public class CommonLineTextView extends RelativeLayout {
+    
+}
