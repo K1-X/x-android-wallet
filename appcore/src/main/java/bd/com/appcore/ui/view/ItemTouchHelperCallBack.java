8d55388c57050640990e9ec16ab3fdd074a94b5e
kolionsleep <kolionsleep@hotmail.com>
2021-04-26 09:00:29 +0800

    new class for touch callback

---
+++ appcore/src/main/java/bd/com/appcore/ui/view/ItemTouchHelperCallBack.java
@@ -0,0 +1,20 @@
+package bd.com.appcore.ui.view;
+
+import android.graphics.Color;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
+
+import com.jcodecraeer.xrecyclerview.XRecyclerView;
+
+import java.util.Collections;
+import java.util.List;
+
+import bd.com.appcore.ui.adapter.CommonAdapter;
+import bd.com.appcore.ui.adapter.MultiItemTypeAdapter;
+
+
+public class ItemTouchHelperCallBack<T> extends ItemTouchHelper.Callback {
+   
+}
