b73150bcc9b3105d88d57e430c0d30002bc4ca0b
madisonpill <madisonpill@hotmail.com>
2021-04-27 09:13:51 +0800

    add LoadingDialog.java

---
+++ LoadingDialog.java
@@ -0,0 +1,14 @@
+package bd.com.appcore.ui.view;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import bd.com.appcore.R;
+
+
+public class LoadingDialog {
+    
+
+}
