12c4048cf3c29ae0ce969e2d15c5724a0d6a5069
kolionsleep <kolionsleep@hotmail.com>
2021-04-28 08:48:48 +0800

    util for gson

---
+++ appcore/src/main/java/bd/com/appcore/util/GsonUtil.java
@@ -0,0 +1,17 @@
+package bd.com.appcore.util;
+
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+
+public class GsonUtil {
+    
+}
