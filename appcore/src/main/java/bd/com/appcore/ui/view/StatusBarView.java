d9ae7369137d3c7c41380ab71bb3f7ee2fa9145b
madisonpill <madisonpill@hotmail.com>
2021-04-29 08:48:11 +0800

    constructor

--- appcore/src/main/java/bd/com/appcore/ui/view/StatusBarView.java
+++ appcore/src/main/java/bd/com/appcore/ui/view/StatusBarView.java
@@ -6,5 +6,12 @@ import android.view.View;
 
 
 public class StatusBarView extends View {
+    public StatusBarView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public StatusBarView(Context context) {
+        super(context);
+    }
 
 }
