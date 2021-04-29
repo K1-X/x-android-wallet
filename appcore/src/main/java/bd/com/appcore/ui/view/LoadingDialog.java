2025b27af86b4dd34b23f07fee9558c8870f9b53
madisonpill <madisonpill@hotmail.com>
2021-04-29 08:46:49 +0800

    loadingDialog

--- appcore/src/main/java/bd/com/appcore/ui/view/LoadingDialog.java
+++ appcore/src/main/java/bd/com/appcore/ui/view/LoadingDialog.java
@@ -13,4 +13,12 @@ public class LoadingDialog {
     private static LoadingDialog mLoading;
     public static Dialog loadingDialog;    
 
+    public static void loadingDialog(Context ct) {
+        cancleDialog();
+        View view = LayoutInflater.from(ct).inflate(R.layout.loading_dialog, null);
+        loadingDialog = new Dialog(ct, R.style.style_loading_dialog);
+        loadingDialog.setContentView(view);
+        loadingDialog.setCanceledOnTouchOutside(false);
+        loadingDialog.show();
+    }
 }
