3dc3e584b5d6c360908ce1ae8efedfe3bd2d2a63
kolionsleep <kolionsleep@hotmail.com>
2021-04-30 09:04:58 +0800

    onlineTimes

--- appcore/src/main/java/bd/com/appcore/util/TimeConstant.java
+++ appcore/src/main/java/bd/com/appcore/util/TimeConstant.java
@@ -12,4 +12,7 @@ public class TimeConstant {
     /** */
     public static List<String> times = new ArrayList<>();
     public static List<GestureTime> timeObjs = new ArrayList<>();    
+
+    public static List<String> onlineTimes = new ArrayList<>();
+    public static List<GestureTime> onlineTimeObjs = new ArrayList<>();
 }
