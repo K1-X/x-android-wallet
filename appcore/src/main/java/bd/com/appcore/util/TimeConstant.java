7e979adf9c3f3247b09aeba8734a67ab4b9ac29d
kolionsleep <kolionsleep@hotmail.com>
2021-04-30 09:05:13 +0800

    some long constants

--- appcore/src/main/java/bd/com/appcore/util/TimeConstant.java
+++ appcore/src/main/java/bd/com/appcore/util/TimeConstant.java
@@ -15,4 +15,13 @@ public class TimeConstant {
 
     public static List<String> onlineTimes = new ArrayList<>();
     public static List<GestureTime> onlineTimeObjs = new ArrayList<>();
+
+    public static final long min_10=10*60*1000;
+    public static final long min_30=30*60*1000;
+    public static final long one_hour=60*60*1000;
+    public static final long six_hour=6*60*60*1000;
+    public static final long one_day=24*60*60*1000;
+    public static final long three_day=3*24*60*60*1000;
+    public static final long seven_day=7*24*60*60*1000;
+    public static final long day_30=30*24*60*60*1000;
 }
