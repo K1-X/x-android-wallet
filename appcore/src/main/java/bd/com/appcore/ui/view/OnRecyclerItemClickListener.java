ef965bad6479312cc4cd3f5c22b6bc4c4a40eeb9
kolionsleep <kolionsleep@hotmail.com>
2021-05-08 08:38:55 +0800

    OnRecyclerItemClickListener.java

---
+++ appcore/src/main/java/bd/com/appcore/ui/view/OnRecyclerItemClickListener.java
@@ -0,0 +1,12 @@
+package bd.com.appcore.ui.view;
+
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+
+public abstract class OnRecyclerItemClickListener implements RecyclerView.OnItemTouchListener {
+   
+}
