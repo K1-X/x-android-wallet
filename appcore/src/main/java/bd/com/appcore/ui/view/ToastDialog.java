5cb3872a2106aed5d1978e760a99746aa97e078b
kolionsleep <kolionsleep@hotmail.com>
2021-05-10 09:04:11 +0800

    new toastDialog.java

---
+++ appcore/src/main/java/bd/com/appcore/ui/view/ToastDialog.java
@@ -0,0 +1,19 @@
+package bd.com.appcore.ui.view;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.wang.avi.AVLoadingIndicatorView;
+
+import bd.com.appcore.CoreApp;
+import bd.com.appcore.R;
+
+
+public class ToastDialog extends Dialog {
+   
+}
