863f46298cd711fd74bda3187699344378215d22
madisonpill <madisonpill@hotmail.com>
2021-05-11 09:13:45 +0800

    time counter

---
+++ appcore/src/main/java/bd/com/appcore/util/TimeCounter.java
@@ -0,0 +1,17 @@
+package bd.com.appcore.util;
+
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *  , getTimeCounter(tag)，tag
+ *
+ */
+public class TimeCounter {
+	
+}
