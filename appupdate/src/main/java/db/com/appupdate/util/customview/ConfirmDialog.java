5e48456f3fe7e07a201335ee5e99cdf468e17602
madisonpill <madisonpill@hotmail.com>
2021-05-13 08:56:01 +0800

    add confirm dialog.java

---
+++ appupdate/src/main/java/db/com/appupdate/util/customview/ConfirmDialog.java
@@ -0,0 +1,18 @@
+package bd.com.appupdate.customview;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import bd.com.appupdate.R;
+import bd.com.appupdate.feature.Callback;
+
+
+public class ConfirmDialog extends Dialog {
+
+    
+
+
+}
