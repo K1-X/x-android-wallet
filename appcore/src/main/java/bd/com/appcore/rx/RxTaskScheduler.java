b9fd11a5681a68bcf6474136d7bdb0e7991f53b6
madisonpill <madisonpill@hotmail.com>
2021-05-13 08:53:35 +0800

    postIoMainTask

--- appcore/src/main/java/bd/com/appcore/rx/RxTaskScheduler.java
+++ appcore/src/main/java/bd/com/appcore/rx/RxTaskScheduler.java
@@ -65,5 +65,63 @@ public class RxTaskScheduler {
                 });
     }
 
+    /**
+     * I/O （、、） Scheduler。
+     *
+     * ，
+     * onStart：
+     * onSuccess：,UI
+     * onFailed：，UI
+     * onComplete：，UI
+     * @param rxTaskCallBack
+     * @param <T>
+     */
+    public static <T> void postIoMainTask(final RxTaskCallBack<T> rxTaskCallBack) {
+        Observable.create(new ObservableOnSubscribe<T>() {
+            @Override
+            public void subscribe(ObservableEmitter<T> e) throws Exception {
+                T t = rxTaskCallBack.doWork();
+                if (t != null) {
+                    e.onNext(t);
+                } else {
+                    postUiTask(new RxTaskCallBack<Boolean>() {
+                        @Override
+                        public Boolean doWork() {
+                            rxTaskCallBack.onSuccess(null);
+                            return true;
+                        }
+                    });
+                }
+                e.onComplete();
+            }
+        })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .safeSubscribe(new Observer<T>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        rxTaskCallBack.onStart(d);
+                    }
+
+                    @Override
+                    public void onNext(T t) {
+                        rxTaskCallBack.onSuccess(t);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        if(e instanceof LogicException){
+                            rxTaskCallBack.onFailed((LogicException) e);
+                        }else {
+                            rxTaskCallBack.onFailed(new LogicException(e, -1));
+                        }
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        rxTaskCallBack.onComplete();
+                    }
+                });
+    }
 }
 
