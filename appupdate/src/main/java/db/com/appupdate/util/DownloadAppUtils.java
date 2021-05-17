c3564c2ae8a317fce910e39bb17ad0c6a727526e
madisonpill <madisonpill@hotmail.com>
2021-05-17 08:37:31 +0800

    add download app utils

---
+++ appupdate/src/main/java/db/com/appupdate/util/DownloadAppUtils.java
@@ -0,0 +1,31 @@
+package bd.com.appupdate.util;
+
+import android.app.AlertDialog;
+import android.app.DownloadManager;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.Settings;
+import android.support.v4.content.FileProvider;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Toast;
+
+
+import com.liulishuo.filedownloader.BaseDownloadTask;
+import com.liulishuo.filedownloader.FileDownloadLargeFileListener;
+import com.liulishuo.filedownloader.FileDownloader;
+
+import java.io.File;
+
+
+public class DownloadAppUtils {
+   
+
+}
