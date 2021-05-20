d3fe01f7433142c0c55e8dee42047edd61640856
madisonpill <madisonpill@hotmail.com>
2021-05-21 05:00:01 +0800

    Md5 class

---
+++ appupdate/src/main/java/db/com/appupdate/util/Md5.java
@@ -0,0 +1,17 @@
+package bd.com.appupdate.util;
+
+import android.content.Context;
+import android.os.Environment;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Md5 {
+
+    
+}
