fd17810f518897c8c6ec7f779e2576dfafe99b56
madisonpill <madisonpill@hotmail.com>
2021-05-21 05:20:01 +0800

    static code block

--- appupdate/src/main/java/db/com/appupdate/util/Md5.java
+++ appupdate/src/main/java/db/com/appupdate/util/Md5.java
@@ -13,5 +13,17 @@ import java.security.NoSuchAlgorithmException;
 
 public class Md5 {
 
+    protected static char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+    protected static MessageDigest messageDigest = null;
+
+    static {
+        try {
+            // MD5（SHA1”SHA1”）
+            messageDigest = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println(Md5.class.getName() + "，MessageDigestMD5Util.");
+            e.printStackTrace();
+        }
+    }
     
 }
