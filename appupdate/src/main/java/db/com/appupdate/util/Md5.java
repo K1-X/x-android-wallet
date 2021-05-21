5d875d3da6760fad32bcef8999c0962064ac92cb
madisonpill <madisonpill@hotmail.com>
2021-05-21 09:20:01 +0800

    getFileMD5

--- appupdate/src/main/java/db/com/appupdate/util/Md5.java
+++ appupdate/src/main/java/db/com/appupdate/util/Md5.java
@@ -103,4 +103,32 @@ public class Md5 {
         }
         return null;
     }
+
+    public static String getFileMD5(File file) {
+        if (!file.isFile()) {
+            return null;
+        }
+        MessageDigest digest = null;
+        FileInputStream in = null;
+        byte buffer[] = new byte[1024];
+        int len;
+        try {
+            digest = MessageDigest.getInstance("MD5");
+            in = new FileInputStream(file);
+            while ((len = in.read(buffer, 0, 1024)) != -1) {
+                digest.update(buffer, 0, len);
+            }
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        BigInteger bigInt = new BigInteger(1, digest.digest());
+        return bigInt.toString(16);
+    }
+
+    public static String test(Context context) {
+        File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/ZhdUncaughtExceptionLog.txt");
+        return getFileMD5(file);
+    }
 }
