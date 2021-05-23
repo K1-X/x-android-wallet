ddb8322d9eb4bfd616f0d7ceca27939a473d6ba2
madisonpill <madisonpill@hotmail.com>
2021-05-24 05:20:01 +0800

    class UpdateAppReceiver

--- appupdate/src/main/java/db/com/appupdate/util/UpdateAppReceiver.java
+++ appupdate/src/main/java/db/com/appupdate/util/UpdateAppReceiver.java
@@ -1,2 +1,23 @@
 package bd.com.appupdate.util;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.FileProvider;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.File;
+
+import bd.com.appcore.rx.RxTaskCallBack;
+import bd.com.appcore.rx.RxTaskScheduler;
+
+public class UpdateAppReceiver extends BroadcastReceiver {
+
+}
