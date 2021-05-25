cc63a5cff2b133d0625210c88778c08398fe6909
madisonpill <madisonpill@hotmail.com>
2021-05-26 05:00:01 +0800

    update app utils.java

---
+++ appupdate/src/main/java/db/com/appupdate/util/UpdateAppUtils.java
@@ -0,0 +1,27 @@
+package bd.com.appupdate.util;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+
+import bd.com.appupdate.R;
+import bd.com.appupdate.customview.AppUpdateDialog;
+import bd.com.appupdate.customview.ConfirmDialog;
+import bd.com.appupdate.feature.Callback;
+
+
+public class UpdateAppUtils {
+    
+
+}
