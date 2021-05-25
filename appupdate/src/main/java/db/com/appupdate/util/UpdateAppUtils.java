bdc2f0c907514456611ad8d9b896d63078ac9870
madisonpill <madisonpill@hotmail.com>
2021-05-26 05:20:01 +0800

    needFitAndroidN

--- appupdate/src/main/java/db/com/appupdate/util/UpdateAppUtils.java
+++ appupdate/src/main/java/db/com/appupdate/util/UpdateAppUtils.java
@@ -22,6 +22,31 @@ import bd.com.appupdate.feature.Callback;
 
 
 public class UpdateAppUtils {
-    
+
+    private OnCancelClicked onCancelClicked;
+    private final String TAG = "UpdateAppUtils";
+    public static final int CHECK_BY_VERSION_NAME = 1001;
+    public static final int CHECK_BY_VERSION_CODE = 1002;
+    public static final int DOWNLOAD_BY_APP = 1003;
+    public static final int DOWNLOAD_BY_BROWSER = 1004;
+
+    private Activity activity;
+    private int checkBy = CHECK_BY_VERSION_CODE;
+    private int downloadBy = DOWNLOAD_BY_APP;
+    // private int serverVersionCode = 0;
+    private String apkPath = "";
+    private String serverVersionName = "";
+    private boolean isForce = false; //
+    private int localVersionCode = 0;
+    private String localVersionName = "";
+    public static boolean needFitAndroidN = true; // 7.0 false
+    public static boolean showNotification = true;
+    private String updateInfo = "";
+    public static String apkDigest;
+
+    public UpdateAppUtils needFitAndroidN(boolean needFitAndroidN) {
+        UpdateAppUtils.needFitAndroidN = needFitAndroidN;
+        return this;
+    }    
 
 }
