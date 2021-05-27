14f48633d8afd6a0306c2a11e9b8493eaaff3d1d
madisonpill <madisonpill@hotmail.com>
2021-05-28 07:20:01 +0800

    confirmdialog.java file

---
+++ appupdate/src/main/java/db/com/appupdate/customview/ConfirmDialog.java
@@ -0,0 +1,12 @@
+package bd.com.appupdate.customview;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import bd.com.appupdate.R;
+import bd.com.appupdate.feature.Callback;
+
+
