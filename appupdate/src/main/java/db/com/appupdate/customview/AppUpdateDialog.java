3bdb802bb00644e49898d2bda1eb3eb9b69dbe77
madisonpill <madisonpill@hotmail.com>
2021-06-01 05:20:01 +0800

    AppUpdateDialog

--- appupdate/src/main/java/db/com/appupdate/customview/AppUpdateDialog.java
+++ appupdate/src/main/java/db/com/appupdate/customview/AppUpdateDialog.java
@@ -1,1 +1,39 @@
 package bd.com.appupdate.customview;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+
+import com.liulishuo.filedownloader.BaseDownloadTask;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+
+import bd.com.appupdate.R;
+import bd.com.appupdate.util.DownLoadCallBack;
+import bd.com.appupdate.util.DownloadAppUtils;
+import io.reactivex.Flowable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+
+public class AppUpdateDialog extends Dialog {
+
+
+}
