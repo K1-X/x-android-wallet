073cbe2d06f52229f6d11f2a54da868a629dfa50
kolionsleep <kolionsleep@hotmail.com>
2021-06-11 07:50:01 +0800

    GreendaoFactory.java added

---
+++ walletdb/src/main/java/bd/com/walletdb/GreendaoFactory.java
@@ -0,0 +1,21 @@
+package bd.com.walletdb;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import org.greenrobot.greendao.database.Database;
+
+import bd.com.walletdb.config.Config;
+import bd.com.walletdb.greendao.DaoMaster;
+import bd.com.walletdb.greendao.DaoSession;
+import bd.com.walletdb.helper.DbHelper;
+
+import static bd.com.walletdb.config.Config.BD_WALLET;
+
+
+public class GreendaoFactory {
+    
+
+}
+
