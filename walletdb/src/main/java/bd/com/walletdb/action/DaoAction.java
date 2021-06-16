bf789c84b3804810015b9a7e4d7e8360fe69db19
kolionsleep <kolionsleep@hotmail.com>
2021-06-16 09:20:01 +0800

    between

--- walletdb/src/main/java/bd/com/walletdb/action/DaoAction.java
+++ walletdb/src/main/java/bd/com/walletdb/action/DaoAction.java
@@ -57,4 +57,27 @@ public interface DaoAction<T> {
      *
      **/
     public List<T> loadAllAsc(Property... properties);  // 
+
+    /**
+     *  Function: loadAllDesc()
+     *      ，
+     *  Params:
+     *      Property... properties, /，ReportEntityDao.Properties.Id
+     *
+     **/
+    public List<T> loadAllDesc(Property... properties);
+
+
+    /* 
+       NOTE: ，（Object value），crash。
+       ，BaseDaoAction，。
+     */
+    public DaoAction<T> eq(Property property, Object value);
+    public DaoAction<T> notEq(Property property, Object value);
+    public DaoAction<T> like(Property property, String value);
+    public DaoAction<T> between(Property property, Object value1, Object value2);
+    public DaoAction<T> gt(Property property, Object value);
+    public DaoAction<T> lt(Property property, Object value);
+    public DaoAction<T> ge(Property property, Object value);
+    public DaoAction<T> le(Property property, Object value);
 }
