45dff3c9383569ae86a517b7ae01171ace055fc0
kolionsleep <kolionsleep@hotmail.com>
2021-06-18 06:30:01 +0800

    DaoSession

--- walletdb/src/main/java/bd/com/walletdb/action/BaseDaoAction.java
+++ walletdb/src/main/java/bd/com/walletdb/action/BaseDaoAction.java
@@ -13,5 +13,16 @@ import bd.com.walletdb.greendao.DaoSession;
 
 public abstract class BaseDaoAction<T, G extends AbstractDao> implements DaoAction<T> {
 
+    private static final String TAG = "BaseDaoAction";
+
+    private static int LOGIC_AND  = 1;
+    private static int LOGIC_OR = 2;
+
+
+    protected abstract QueryBuilder<T> getQueryBuilder();
+
+    protected abstract G getEntityDao();
+
+    protected abstract DaoSession getDaoSession();
     
 }
