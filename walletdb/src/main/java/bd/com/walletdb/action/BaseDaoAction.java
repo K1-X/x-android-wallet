b86c89953f6382585693ce99acf4f8fe14631c23
kolionsleep <kolionsleep@hotmail.com>
2021-06-18 05:50:01 +0800

    BaseDaoAction.java

---
+++ walletdb/src/main/java/bd/com/walletdb/action/BaseDaoAction.java
@@ -0,0 +1,17 @@
+package bd.com.walletdb.action;
+
+import org.greenrobot.greendao.AbstractDao;
+import org.greenrobot.greendao.Property;
+import org.greenrobot.greendao.query.QueryBuilder;
+import org.greenrobot.greendao.query.WhereCondition;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import bd.com.walletdb.greendao.DaoSession;
+
+
+public abstract class BaseDaoAction<T, G extends AbstractDao> implements DaoAction<T> {
+
+    
+}
