0b8a05b3e52e7d7b8c117c466b09270ae45948a7
madisonpill <madisonpill@hotmail.com>
2021-06-23 05:00:01 +0800

    ThreadManager.java added

---
+++ app/src/main/java/bd/com/bdwallet/app/ThreadManager.java
@@ -0,0 +1,15 @@
+package bd.com.bdwallet.app;
+
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadManager
+{
+    
+}
