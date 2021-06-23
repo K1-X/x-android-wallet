726ae6670fe724c3cb4a4cb5737fca54626f95e6
kolionsleep <kolionsleep@hotmail.com>
2021-06-24 05:50:01 +0800

    DouTxAction.java added

---
+++ walletdb/src/main/java/bd/com/walletdb/action/DouTxAction.java
@@ -0,0 +1,12 @@
+package bd.com.walletdb.action;
+
+import org.greenrobot.greendao.query.QueryBuilder;
+
+import bd.com.walletdb.GreendaoFactory;
+import bd.com.walletdb.entity.DoubleTxEntity;
+import bd.com.walletdb.greendao.DaoSession;
+import bd.com.walletdb.greendao.DoubleTxEntityDao;
+
+public class DouTxAction extends BaseDaoAction<DoubleTxEntity, DoubleTxEntityDao> {
+
+}
