865a11da029e586f7a6643bc2b8c21ab3a4b5546
kolionsleep <kolionsleep@hotmail.com>
2021-06-24 09:00:01 +0800

    ContractAction

---
+++ walletdb/src/main/java/bd/com/walletdb/action/ContactAction.java
@@ -0,0 +1,12 @@
+package bd.com.walletdb.action;
+
+import org.greenrobot.greendao.query.QueryBuilder;
+
+import bd.com.walletdb.GreendaoFactory;
+import bd.com.walletdb.entity.ContactEntity;
+import bd.com.walletdb.greendao.ContactEntityDao;
+import bd.com.walletdb.greendao.DaoSession;
+
+public class ContactAction extends BaseDaoAction<ContactEntity,ContactEntityDao> {
+    
+}
