25ae0e5bf9fc085b0ef8f7ff89e7896822d44b5f
madisonpill <madisonpill@hotmail.com>
2021-06-25 07:20:01 +0800

    onCreate

--- app/src/main/java/bd/com/bdwallet/app/BdApplication.java
+++ app/src/main/java/bd/com/bdwallet/app/BdApplication.java
@@ -30,5 +30,38 @@ import cn.jpush.android.api.JPushInterface;
 
 public class BdApplication extends CoreApp {
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AppFilePath.init(this);
+        CustomerException.getExceptionControl().init(context);
+        AppSettings.getAppSettings().init(this);
+        GreendaoFactory.make(this);
+        GreendaoFactory.makeTx(this);
+        GreendaoFactory.makeContact(this);
+        NoHttp.initialize(this);
+        Logger.setDebug(true);// NoHttp, 、。
+        Logger.setTag("Nohttp");
+        resetDB();
+
+        initChanin();
+        //jpush
+        JPushInterface.setDebugMode(true);    // ,
+        JPushInterface.init(this);            //  JPush
+        //JPUSH
+        PushManager.init(this);
+        initBugly();
+        AppDataInitor.getInitor().init();
+        // CoinService.getService().start();
+        BlockNumberAsync.getAsync().init();
+        EnvModel.getInstance().init();
+        /**
+         * 
+         */
+        registerActivityLifecycleCallbacks(new LockPatternCallback());
+        initUmeng();
+        QbSdk.initX5Environment(this,null);
+        Log.e("location", LocationUtils.sHA1(this));
+    }
     
 }
