ad2a4b25306a7c3c6231e9059fd56ddd138d18ed
kolionsleep <kolionsleep@hotmail.com>
2021-06-28 06:30:01 +0800

    PriceAction.java added

---
+++ walletdb/src/main/java/bd/com/walletdb/action/PriceAction.java
@@ -0,0 +1,12 @@
+package bd.com.walletdb.action;
+
+import org.greenrobot.greendao.query.QueryBuilder;
+
+import bd.com.walletdb.GreendaoFactory;
+import bd.com.walletdb.entity.Price;
+import bd.com.walletdb.greendao.DaoSession;
+import bd.com.walletdb.greendao.PriceDao;
+
+public class PriceAction extends BaseDaoAction<Price, PriceDao> {
+    
+}
