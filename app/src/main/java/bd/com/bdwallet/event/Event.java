17ea74efb1da740a68853b98660a95076809b92e
madisonpill <madisonpill@hotmail.com>
2021-07-01 05:50:01 +0800

    Event.java added

---
+++ app/src/main/java/bd/com/bdwallet/event/Event.java
@@ -0,0 +1,11 @@
+package bd.com.bdwallet.event;
+
+
+import org.w3c.dom.Node;
+import org.web3j.crypto.Wallet;
+import org.web3j.protocol.core.methods.request.Transaction;
+
+public class Event {
+
+    
+}
