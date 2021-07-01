1fde8779e60c1f1aa7f683c94998f861780e68e3
kolionsleep <kolionsleep@hotmail.com>
2021-07-02 07:20:01 +0800

    TxHistoryAction.java acced

---
+++ walletdb/src/main/java/bd/com/walletdb/action/TxHistoryAction.java
@@ -0,0 +1,15 @@
+package bd.com.walletdb.action;
+
+import org.greenrobot.greendao.query.QueryBuilder;
+
+import bd.com.walletdb.GreendaoFactory;
+import bd.com.walletdb.entity.TransactionEntity;
+import bd.com.walletdb.entity.TxHistory;
+import bd.com.walletdb.greendao.DaoSession;
+import bd.com.walletdb.greendao.TransactionEntityDao;
+import bd.com.walletdb.greendao.TxHistoryDao;
+
+
+public class TxHistoryAction extends BaseDaoAction<TxHistory,TxHistoryDao> {
+    
+}
