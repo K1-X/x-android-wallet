9276f2bf23136b103a83cb20b023a01026a0af6c
madisonpill <madisonpill@hotmail.com>
2021-07-05 05:50:01 +0800

    EventPublisher.java added

---
+++ app/src/main/java/bd/com/bdwallet/event/EventPublisher.java
@@ -0,0 +1,13 @@
+package bd.com.bdwallet.event;
+
+import android.content.Context;
+
+import org.w3c.dom.Node;
+import org.web3j.crypto.Wallet;
+import org.web3j.protocol.core.methods.request.Transaction;
+
+
+public class EventPublisher {
+
+  
+}
