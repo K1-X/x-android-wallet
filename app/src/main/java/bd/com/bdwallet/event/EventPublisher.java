25bbc92550cc4cbaf0e73ae59e1edb6650ae7770
madisonpill <madisonpill@hotmail.com>
2021-07-05 10:10:01 +0800

    sendUpdateDelegateTabEvent

--- app/src/main/java/bd/com/bdwallet/event/EventPublisher.java
+++ app/src/main/java/bd/com/bdwallet/event/EventPublisher.java
@@ -69,4 +69,16 @@ private static final String TAG = "Portal.EventPublisher";
     public void sendWalletListOrderChangedEvent() {
         BusProvider.post(new Event.WalletListOrderChangedEvent());
     }
+
+    public void sendUpdateDelegateEvent() {
+        BusProvider.post(new Event.UpdateDelegateDetailEvent());
+    }
+
+    public void sendUpdateValidatorsDetailEvent() {
+        BusProvider.post(new Event.UpdateValidatorsDetailEvent());
+    }
+
+    public void sendUpdateDelegateTabEvent() {
+        BusProvider.post(new Event.UpdateDelegateTabEvent());
+    }
 }
