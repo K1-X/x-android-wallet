04aebcceaa8c73e5acb7e1b7235a7c6a4d93afb8
madisonpill <madisonpill@hotmail.com>
2021-07-07 11:40:01 +0800

    getAddrFromCode

--- app/src/main/java/bd/com/bdwallet/util/AddressUtil.java
+++ app/src/main/java/bd/com/bdwallet/util/AddressUtil.java
@@ -3,5 +3,12 @@ package bd.com.bdwallet.util;
 import android.text.TextUtils;
 
 public class AddressUtil {
-   
+
+    public static String getAddrFromCode(String code) {
+        if (TextUtils.isEmpty(code)) return "";
+        String result = code;
+        String res[] = result.split("0x");
+        String addr = "0x" + res[1];
+        return addr;
+    }   
 }
