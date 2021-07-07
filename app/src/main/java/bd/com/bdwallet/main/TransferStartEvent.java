f6e23a81ebf99596372f972a9831f771f2a32634
madisonpill <madisonpill@hotmail.com>
2021-07-07 09:00:01 +0800

    setHistory

--- app/src/main/java/bd/com/bdwallet/main/TransferStartEvent.java
+++ app/src/main/java/bd/com/bdwallet/main/TransferStartEvent.java
@@ -4,5 +4,14 @@ import bd.com.walletdb.entity.TxHistory;
 
 
 public class TransferStartEvent {
-    
+
+    private TxHistory history;
+
+    public TxHistory getHistory() {
+        return history;
+    }
+
+    public void setHistory(TxHistory history) {
+        this.history = history;
+    }    
 }
