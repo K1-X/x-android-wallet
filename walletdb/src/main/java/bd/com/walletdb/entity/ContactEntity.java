c5901b3747c6581033be092357e7698f0cd9bae4
kolionsleep <kolionsleep@hotmail.com>
2021-07-08 06:30:01 +0800

    ContactEntity.java added

---
+++ walletdb/src/main/java/bd/com/walletdb/entity/ContactEntity.java
@@ -0,0 +1,13 @@
+package bd.com.walletdb.entity;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Generated;
+
+@Entity
+public class ContactEntity implements Parcelable {
+    
+}
