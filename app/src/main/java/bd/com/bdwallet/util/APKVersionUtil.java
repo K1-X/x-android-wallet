074bdfb5e5719cbd19a50c3d64bec77a65edd702
madisonpill <madisonpill@hotmail.com>
2021-07-09 09:20:01 +0800

    getVersionCode

--- app/src/main/java/bd/com/bdwallet/util/APKVersionUtil.java
+++ app/src/main/java/bd/com/bdwallet/util/APKVersionUtil.java
@@ -8,5 +8,22 @@ import bd.com.bdwallet.app.BdApplication;
 
 
 public class APKVersionUtil {
-    
+
+    /**
+     * apk
+     *
+     * @param mContext
+     * @return
+     */
+    public static int getVersionCode(Context mContext) {
+        int versionCode = 0;
+        try {
+            //，AndroidManifest.xmlandroid:versionCode
+            versionCode = mContext.getPackageManager().
+                    getPackageInfo(mContext.getPackageName(), 0).versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return versionCode;
+    }    
 }
