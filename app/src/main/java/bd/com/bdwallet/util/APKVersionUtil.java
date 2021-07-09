c571e0cbddb72a171164324a7508978cf21a7ec1
madisonpill <madisonpill@hotmail.com>
2021-07-09 09:00:01 +0800

    APKVersionUtil.java added

---
+++ app/src/main/java/bd/com/bdwallet/util/APKVersionUtil.java
@@ -0,0 +1,12 @@
+package bd.com.bdwallet.util;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+
+import bd.com.bdwallet.Config;
+import bd.com.bdwallet.app.BdApplication;
+
+
+public class APKVersionUtil {
+    
+}
