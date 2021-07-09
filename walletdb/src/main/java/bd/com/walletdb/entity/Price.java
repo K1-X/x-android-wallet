3ec97cb7817671814a6d1fb7b56ba27fbcaf7fca
kolionsleep <kolionsleep@hotmail.com>
2021-07-10 06:30:01 +0800

    Price

--- walletdb/src/main/java/bd/com/walletdb/entity/Price.java
+++ walletdb/src/main/java/bd/com/walletdb/entity/Price.java
@@ -10,5 +10,28 @@ import org.greenrobot.greendao.annotation.Index;
 
 @Entity(indexes = {@Index(value = "address,chainId", unique = true)})
 public class Price implements Parcelable {
-    
+
+    @Id(autoincrement = true)
+    private Long id;
+    private String marketName;
+    private String asks;
+    private String bids;
+    private String unit;
+    private String address;
+    private String chainId;
+
+
+    protected Price(Parcel in) {
+        if (in.readByte() == 0) {
+            id = null;
+        } else {
+            id = in.readLong();
+        }
+        marketName = in.readString();
+        asks = in.readString();
+        bids = in.readString();
+        unit = in.readString();
+        address = in.readString();
+        chainId = in.readString();
+    }    
 }
