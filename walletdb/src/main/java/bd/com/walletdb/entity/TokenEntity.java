b1ade6dfac79b9bdd99180223fab2051be6633ea
kolionsleep <kolionsleep@hotmail.com>
2021-07-13 05:20:01 +0800

    TokenEntity.java

---
+++ walletdb/src/main/java/bd/com/walletdb/entity/TokenEntity.java
@@ -0,0 +1,15 @@
+package bd.com.walletdb.entity;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Generated;
+import org.greenrobot.greendao.annotation.Index;
+
+
+@Entity(indexes = {@Index(value = "address,walletAddress", unique = true)})
+public class TokenEntity implements Parcelable {
+    
+}
