8b927d677233f08dc53d79d699ab92e823b2ae55
kolionsleep <kolionsleep@hotmail.com>
2021-07-13 05:50:01 +0800

    walletAddress

--- walletdb/src/main/java/bd/com/walletdb/entity/TokenEntity.java
+++ walletdb/src/main/java/bd/com/walletdb/entity/TokenEntity.java
@@ -11,5 +11,21 @@ import org.greenrobot.greendao.annotation.Index;
 
 @Entity(indexes = {@Index(value = "address,walletAddress", unique = true)})
 public class TokenEntity implements Parcelable {
-    
+
+    @Id(autoincrement = true)
+    private Long id;
+    private String address;
+    private String icon;//(bas e64 )
+    private String name; //
+    private String symbol; //
+    private String supplyTotal;  //
+    private int decimals; //，:=181token=1000000000000000000
+    private String version; //
+    private boolean checked; //
+    private String publisher;//
+    private String chainId;//Id
+    private String balance;//
+    private String value;// =balance*price
+    private String price;//
+    private String walletAddress;//    
 }
