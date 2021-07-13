8e65886f5ad211d2c57bcddaabc3b72fecd67753
madisonpill <madisonpill@hotmail.com>
2021-07-14 07:50:01 +0800

    AssetsDbManager.java

---
+++ app/src/main/java/bd/com/bdwallet/util/AssetsDbManager.java
@@ -0,0 +1,24 @@
+package bd.com.bdwallet.util;
+
+import android.content.res.AssetManager;
+import android.util.Log;
+
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import bd.com.appcore.util.GsonUtil;
+import bd.com.bdwallet.app.BdApplication;
+import bd.com.bdwallet.module.news.domain.News;
+
+
+public class AssetsDbManager {
+    
+
+}
