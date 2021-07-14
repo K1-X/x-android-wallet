a0dd4031f6db9eb3f129933a1ececd7e082d93da
kolionsleep <kolionsleep@hotmail.com>
2021-07-15 05:20:01 +0800

    private attrs

--- walletdb/src/main/java/bd/com/walletdb/entity/TransactionDetail.java
+++ walletdb/src/main/java/bd/com/walletdb/entity/TransactionDetail.java
@@ -6,5 +6,26 @@ import org.greenrobot.greendao.annotation.Generated;
 
 @Entity
 public class TransactionDetail {
-    
+
+    @Id
+    private String pkHash;
+    private String blockHash;
+    private long blockNumber;
+    private String blockTimestamp;
+    private String blockGasLimit;
+    private String transactionIndex;
+    private String transactionFrom;
+    private String transactionTo;
+    private long gas;
+    private long gasUsed;
+    private long gasPrice;
+    private long cumulativeGas;
+    private String randomId;
+    private String inputText;
+    private long lastBlockNumber;
+    private String txCost;
+    private String value;
+    private String tokenTransferTo;
+    private String tokenTransfer;
+    private int type;    
 }
