1ec9ced57536b00b39c1f0ab3a1bad967a0942c0
kolionsleep <kolionsleep@hotmail.com>
2021-07-15 05:00:01 +0800

    TransactionDetail.java

---
+++ walletdb/src/main/java/bd/com/walletdb/entity/TransactionDetail.java
@@ -0,0 +1,10 @@
+package bd.com.walletdb.entity;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Generated;
+
+@Entity
+public class TransactionDetail {
+    
+}
