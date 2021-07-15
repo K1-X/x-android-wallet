19e91c7c72b965b90975a064a18814b2cf21cac5
kolionsleep <kolionsleep@hotmail.com>
2021-07-15 11:40:01 +0800

    setTokenTransfer

--- walletdb/src/main/java/bd/com/walletdb/entity/TransactionDetail.java
+++ walletdb/src/main/java/bd/com/walletdb/entity/TransactionDetail.java
@@ -137,4 +137,88 @@ public class TransactionDetail {
     public long getGasUsed() {
         return gasUsed;
     }
+
+    public void setGasUsed(long gasUsed) {
+        this.gasUsed = gasUsed;
+    }
+
+    public long getGasPrice() {
+        return gasPrice;
+    }
+
+    public void setGasPrice(long gasPrice) {
+        this.gasPrice = gasPrice;
+    }
+
+    public long getCumulativeGas() {
+        return cumulativeGas;
+    }
+
+    public void setCumulativeGas(long cumulativeGas) {
+        this.cumulativeGas = cumulativeGas;
+    }
+
+    public String getRandomId() {
+        return randomId;
+    }
+
+    public void setRandomId(String randomId) {
+        this.randomId = randomId;
+    }
+
+    public String getInputText() {
+        return inputText;
+    }
+
+    public void setInputText(String inputText) {
+        this.inputText = inputText;
+    }
+
+    public long getLastBlockNumber() {
+        return lastBlockNumber;
+    }
+
+    public void setLastBlockNumber(long lastBlockNumber) {
+        this.lastBlockNumber = lastBlockNumber;
+    }
+
+    public String getTxCost() {
+        return txCost;
+    }
+
+    public void setTxCost(String txCost) {
+        this.txCost = txCost;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getTokenTransferTo() {
+        return tokenTransferTo;
+    }
+
+    public void setTokenTransferTo(String tokenTransferTo) {
+        this.tokenTransferTo = tokenTransferTo;
+    }
+
+    public String getTokenTransfer() {
+        return tokenTransfer;
+    }
+
+    public void setTokenTransfer(String tokenTransfer) {
+        this.tokenTransfer = tokenTransfer;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
 }
