8223e623795904030543af25bdca3635277cb6d7
madisonpill <madisonpill@hotmail.com>
2021-07-16 10:10:01 +0800

    dateToStr

--- app/src/main/java/bd/com/bdwallet/util/DateKit.java
+++ app/src/main/java/bd/com/bdwallet/util/DateKit.java
@@ -141,4 +141,71 @@ public class DateKit {
         }
         return date;
     }
+
+    /**
+     * :yyyy-MM-dd HH:mm:ss
+     */
+    public static Date strToTime(String str) {
+        Date date = null;
+        try {
+            date = timeFormat.get().parse(str);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return date;
+    }
+
+    public static String transformDate(String str) {
+        Date date = strToDate(str);
+        if (date == null)
+            return ":)";
+        String time = new SimpleDateFormat("MMdd").format(date);
+        return time;
+    }
+
+    /**
+     * :
+     * ：；
+     * <p>
+     * ：f ；
+     * <p>
+     * ：h ；
+     * <p>
+     * ：d ；
+     * <p>
+     * ：12-27 22:33；
+     * <p>
+     * ：2014-12-03 22:33；
+     */
+    public static String friendlyFormat(String str) {
+        Date date = strToTime(str);
+        if (date == null)
+            return ":)";
+        Calendar now = getCal();
+        String time = new SimpleDateFormat("HH:mm").format(date);
+
+        // ，
+        String curDate = dateFormat.get().format(now.getTime());//
+        String paramDate = dateFormat.get().format(date);//
+        if (curDate.equals(paramDate)) {//
+            int hour = (int) ((now.getTimeInMillis() - date.getTime()) / 3600000);
+            if (hour > 0)
+                return hour + "";
+            int minute = (int) ((now.getTimeInMillis() - date.getTime()) / 60000);
+            if (minute < 1)
+                return "";
+            return minute + "";
+        }
+
+        // ，
+        int days = (int) ((getBegin(getDate()).getTime() - getBegin(date).getTime()) / 86400000);
+        int hour = (int) ((now.getTimeInMillis() - date.getTime()) / 3600000);
+        if (hour > 0 && hour < 24)
+            return hour + "";
+        if (days < 30 && days > 0)
+            return days + "";
+        if (days >= 30 && days < 365)
+            return dateToStr(date).substring(5);
+        return dateToStr(date);
+    }
 }
