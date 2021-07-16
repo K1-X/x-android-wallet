27a63f3622a3ceb26bcf9e0643e6bd2a004f6666
madisonpill <madisonpill@hotmail.com>
2021-07-16 11:40:01 +0800

    timeStamp

--- app/src/main/java/bd/com/bdwallet/util/DateKit.java
+++ app/src/main/java/bd/com/bdwallet/util/DateKit.java
@@ -208,4 +208,132 @@ public class DateKit {
             return dateToStr(date).substring(5);
         return dateToStr(date);
     }
+
+    /**
+     * 0:2012-07-07 20:20:20 --> 2012-07-07 00:00:00
+     */
+    public static Date getBegin(Date date) {
+        return strToTime(dateToStr(date) + " 00:00:00");
+    }
+
+    public static void main(String[] args) {
+        System.err.println(friendlyFormat("2013-09-16 11:27:19"));
+    }
+
+    public static final long DAY = 86400000;
+
+
+    public static String getHMTime(String date) {
+        try {
+            String str = date.toString().trim();
+            return str.substring(11, 16);
+        } catch (Exception e) {
+            return date;
+        }
+    }
+
+    public static boolean isRightDate(String dataStr) {
+        try {
+            Date nowdate = new Date();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.CHINA);
+            Date d = sdf.parse(dataStr);
+
+            boolean flag = d.before(nowdate);
+            return flag;
+        } catch (Exception e) {
+            return false;
+        }
+
+    }
+
+    public static boolean isRightDate3(String dataStr) {
+        if (TextUtils.isEmpty(dataStr)) return false;
+
+        if (dataStr.length() < 12) {
+            dataStr = dataStr + " 23:59:59";
+        }
+        try {
+            Date nowdate = new Date();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.CHINA);
+            Date d = sdf.parse(dataStr);
+
+            boolean flag = d.before(nowdate);
+            return flag;
+        } catch (Exception e) {
+            return false;
+        }
+
+    }
+
+    public static boolean isRightDate2(String dataStr) {
+        try {
+            Date nowdate = new Date();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM", Locale.CHINA);
+            Date d = sdf.parse(dataStr);
+
+            boolean flag = d.before(nowdate);
+            return flag;
+        } catch (Exception e) {
+            return false;
+        }
+
+    }
+
+    /**
+     * 
+     *
+     * @param seconds 
+     * @param format
+     * @return
+     */
+    public static String timeStamp2Date(String seconds, String format) {
+        if (seconds == null || seconds.isEmpty() || seconds.equals("null")) {
+            return "";
+        }
+        if (format == null || format.isEmpty()) {
+            format = "yyyy-MM-dd HH:mm:ss";
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        return sdf.format(new Date(Long.valueOf(seconds + "000")));
+    }
+
+    /**
+     * 
+     *
+     * @param seconds 
+     * @return
+     */
+    public static String timeStampDate(long seconds) {
+        String format = "yyyy-MM-dd";
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        return sdf.format(new Date(Long.valueOf(seconds)));
+    }
+
+    /**
+     * 
+     *
+     * @param date_str   
+     * @param format ：yyyy-MM-dd HH:mm:ss
+     * @return
+     */
+    public static String date2TimeStamp(String date_str, String format) {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat(format);
+            return String.valueOf(sdf.parse(date_str).getTime() / 1000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    /**
+     * （）
+     *
+     * @return
+     */
+    public static String timeStamp() {
+        long time = System.currentTimeMillis();
+        String t = String.valueOf(time / 1000);
+        return t;
+    }
 }
