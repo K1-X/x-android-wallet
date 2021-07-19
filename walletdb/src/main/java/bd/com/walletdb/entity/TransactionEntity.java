a124892abcec7f04c49aab043cce9f8a87376037
kolionsleep <kolionsleep@hotmail.com>
2021-07-20 06:30:01 +0800

    TransactionEntity.java

---
+++ walletdb/src/main/java/bd/com/walletdb/entity/TransactionEntity.java
@@ -0,0 +1,11 @@
+package bd.com.walletdb.entity;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Generated;
+
+
+@Entity
+public class TransactionEntity {
+    
+}
