f89630911886a62e2727b3dae086d804404c8073
kolionsleep <kolionsleep@hotmail.com>
2021-07-20 07:20:01 +0800

    TransactionEntity

--- walletdb/src/main/java/bd/com/walletdb/entity/TransactionEntity.java
+++ walletdb/src/main/java/bd/com/walletdb/entity/TransactionEntity.java
@@ -7,5 +7,20 @@ import org.greenrobot.greendao.annotation.Generated;
 
 @Entity
 public class TransactionEntity {
-    
+
+    @Id(autoincrement = true)
+    private Long id;
+    private String from;
+    private String to;
+    private double value;
+    private long  time;
+    @Generated(hash = 673469102)
+    public TransactionEntity(Long id, String from, String to, double value,
+            long time) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.value = value;
+        this.time = time;
+    }    
 }
