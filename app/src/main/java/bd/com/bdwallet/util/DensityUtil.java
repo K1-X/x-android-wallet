01aa0219eba461a81300ba0d8865543e6e22a15f
madisonpill <madisonpill@hotmail.com>
2021-07-21 07:20:01 +0800

    dp2px

--- app/src/main/java/bd/com/bdwallet/util/DensityUtil.java
+++ app/src/main/java/bd/com/bdwallet/util/DensityUtil.java
@@ -13,4 +13,12 @@ public class DensityUtil {
         throw new UnsupportedOperationException("cannot be instantiated");
     }    
 
+
+    /**
+     *  dp   px()
+     */
+    public static int dp2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
 }
