90d86de3d9212922045c784806de2f8438c810cd
madisonpill <madisonpill@hotmail.com>
2021-07-21 05:50:01 +0800

    DensityUtil.java

---
+++ app/src/main/java/bd/com/bdwallet/util/DensityUtil.java
@@ -0,0 +1,14 @@
+package bd.com.bdwallet.util;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+
+public class DensityUtil {
+
+    
+
+}
