191f137d15039de916d32771cb97c66018de62b1
kolionsleep <kolionsleep@hotmail.com>
2021-07-22 05:20:01 +0800

    TxHistory

--- walletdb/src/main/java/bd/com/walletdb/entity/TxHistory.java
+++ walletdb/src/main/java/bd/com/walletdb/entity/TxHistory.java
@@ -11,5 +11,50 @@ import org.greenrobot.greendao.annotation.Generated;
 @Entity
 public class TxHistory implements Parcelable {
 
+     @Id
+    private String pkHash;
+    private String blockHash;
+    private int blockNumber;
+    private String blockTimesStr;
+    private String blockGasLimit;
+    private String transactionIndex;
+    private String transactionFrom;
+    private String transactionTo;
+    private String value;
+    private String gas;
+    private String gasPrice;
+    private String cumulativeGas;
+    private int type;
+    private String tokenTransferTo;//
+    private String tokenTransfer;//
+    private String address;//tokenAddr;
+    private String walletAddr;
+    private int lastBlockNumber;
+    private String chainId;
+    private int state;//0 ,1,,2 pengding
+
+
+    protected TxHistory(Parcel in) {
+        pkHash = in.readString();
+        blockHash = in.readString();
+        blockNumber = in.readInt();
+        blockTimesStr = in.readString();
+        blockGasLimit = in.readString();
+        transactionIndex = in.readString();
+        transactionFrom = in.readString();
+        transactionTo = in.readString();
+        value = in.readString();
+        gas = in.readString();
+        gasPrice = in.readString();
+        cumulativeGas = in.readString();
+        type = in.readInt();
+        tokenTransferTo = in.readString();
+        tokenTransfer = in.readString();
+        address = in.readString();
+        walletAddr = in.readString();
+        lastBlockNumber = in.readInt();
+        chainId = in.readString();
+        state = in.readInt();
+    }
     
 }
