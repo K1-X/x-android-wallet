e75411e1f9cf3ad3d6c16643e8c673040527da34
madisonpill <madisonpill@hotmail.com>
2021-07-23 07:50:01 +0800

    FakeDataHelper.java

---
+++ app/src/main/java/bd/com/bdwallet/util/FakeDataHelper.java
@@ -0,0 +1,13 @@
+package bd.com.bdwallet.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import bd.com.appcore.util.AppSettings;
+import bd.com.bdwallet.module.common.GlobConfig;
+import bd.com.walletdb.entity.TokenEntity;
+
+
+public class FakeDataHelper {
+    
+}
