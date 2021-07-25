a42e675c978378ec6b0dfbdbdd1a5ce0c4cbb96f
kolionsleep <kolionsleep@hotmail.com>
2021-07-26 05:20:01 +0800

    WalletEntity.java

---
+++ walletdb/src/main/java/bd/com/walletdb/entity/WalletEntity.java
@@ -0,0 +1,15 @@
+package bd.com.walletdb.entity;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.text.TextUtils;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Generated;
+
+
+@Entity
+public class WalletEntity implements Parcelable {
+    
+}
