505198319985c780eaee3f00e01af54ca495c163
madisonpill <madisonpill@hotmail.com>
2021-07-27 07:50:01 +0800

    FixAppBarLayoutBehavior.java

---
+++ app/src/main/java/bd/com/bdwallet/util/FixAppBarLayoutBehavior.java
@@ -0,0 +1,14 @@
+package bd.com.bdwallet.util;
+
+import android.content.Context;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class FixAppBarLayoutBehavior extends AppBarLayout.Behavior {
+
+    
+
+}
