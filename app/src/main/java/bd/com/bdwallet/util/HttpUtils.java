e46e24f5d02d573cebcd41fb237d1b5cc97067ea
madisonpill <madisonpill@hotmail.com>
2021-07-29 07:50:01 +0800

    HttpUtils.java

---
+++ app/src/main/java/bd/com/bdwallet/util/HttpUtils.java
@@ -0,0 +1,12 @@
+package bd.com.bdwallet.util;
+
+import android.util.Log;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+public class HttpUtils {
+    
+}
