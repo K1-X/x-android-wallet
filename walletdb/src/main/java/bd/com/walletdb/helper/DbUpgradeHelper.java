dd03d71976a365c2b5ab665f66fcdc2af8885eb0
kolionsleep <kolionsleep@hotmail.com>
2021-07-28 09:20:01 +0800

    execSQL

--- walletdb/src/main/java/bd/com/walletdb/helper/DbUpgradeHelper.java
+++ walletdb/src/main/java/bd/com/walletdb/helper/DbUpgradeHelper.java
@@ -38,4 +38,56 @@ public class DbUpgradeHelper {
         //
         restoreData(db, daoClasses);
     }
+
+    private void generateTempTables(Database db, Class<? extends AbstractDao<?, ?>>... daoClasses) {
+        for(int i = 0; i < daoClasses.length; i++) {
+            DaoConfig daoConfig = new DaoConfig(db, daoClasses[i]);
+
+            String divider = "";
+            String tableName = daoConfig.tablename;
+            String tempTableName = daoConfig.tablename.concat("_TEMP");
+            ArrayList<String> properties = new ArrayList<>();
+
+            StringBuilder createTableStringBuilder = new StringBuilder();
+
+            createTableStringBuilder.append("CREATE TABLE ").append(tempTableName).append(" (");
+
+            for(int j = 0; j < daoConfig.properties.length; j++) {
+                String columnName = daoConfig.properties[j].columnName;
+
+                if(getColumns(db, tableName).contains(columnName)) {
+                    properties.add(columnName);
+
+                    String type = null;
+
+                    try {
+                        type = getTypeByClass(daoConfig.properties[j].type);
+                    } catch (Exception exception) {
+                        //Crashlytics.logException(exception);
+                    }
+
+                    createTableStringBuilder.append(divider).append(columnName).append(" ").append(type);
+
+                    if(daoConfig.properties[j].primaryKey) {
+                        createTableStringBuilder.append(" PRIMARY KEY");
+                    }
+
+                    divider = ",";
+                }
+            }
+            createTableStringBuilder.append(");");
+
+            db.execSQL(createTableStringBuilder.toString());
+
+            StringBuilder insertTableStringBuilder = new StringBuilder();
+
+            insertTableStringBuilder.append("INSERT INTO ").append(tempTableName).append(" (");
+            insertTableStringBuilder.append(TextUtils.join(",", properties));
+            insertTableStringBuilder.append(") SELECT ");
+            insertTableStringBuilder.append(TextUtils.join(",", properties));
+            insertTableStringBuilder.append(" FROM ").append(tableName).append(";");
+
+            db.execSQL(insertTableStringBuilder.toString());
+        }
+    }
 }
