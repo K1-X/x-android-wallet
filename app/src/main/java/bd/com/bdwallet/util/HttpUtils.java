5c06c8332c78d4655eba9b4674c940dbbb7c9637
madisonpill <madisonpill@hotmail.com>
2021-07-29 09:00:01 +0800

    createOkHttpClient

--- app/src/main/java/bd/com/bdwallet/util/HttpUtils.java
+++ app/src/main/java/bd/com/bdwallet/util/HttpUtils.java
@@ -8,5 +8,13 @@ import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 
 public class HttpUtils {
-    
+
+    public static OkHttpClient createOkHttpClient() {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.connectTimeout(15_1000, TimeUnit.MILLISECONDS);
+        builder.writeTimeout(15_1000, TimeUnit.MILLISECONDS);
+        builder.readTimeout(15_1000, TimeUnit.MILLISECONDS);
+        configureLogging(builder);
+        return builder.build();
+    }    
 }
