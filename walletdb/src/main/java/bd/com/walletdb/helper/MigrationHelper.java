be09e195285569d95f45624ff0a13c99dae1b200
kolionsleep <kolionsleep@hotmail.com>
2021-08-02 05:50:01 +0800

    MigrationHelper

---
+++ walletdb/src/main/java/bd/com/walletdb/helper/MigrationHelper.java
@@ -0,0 +1,30 @@
+package bd.com.walletdb.helper;
+
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.util.Log;
+
+import org.greenrobot.greendao.AbstractDao;
+import org.greenrobot.greendao.database.Database;
+import org.greenrobot.greendao.database.StandardDatabase;
+import org.greenrobot.greendao.internal.DaoConfig;
+
+import java.lang.ref.WeakReference;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+/**
+ * GreenDao，
+ */
+public final class MigrationHelper {
+
+    
+
+}
