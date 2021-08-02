a8ebdd3e020b6b0d4834a16bf0ca4c6b9a4925b2
kolionsleep <kolionsleep@hotmail.com>
2021-08-02 11:40:01 +0800

    getColumns

--- walletdb/src/main/java/bd/com/walletdb/helper/MigrationHelper.java
+++ walletdb/src/main/java/bd/com/walletdb/helper/MigrationHelper.java
@@ -163,4 +163,87 @@ public final class MigrationHelper {
         reflectMethod(db, "createTable", ifNotExists, daoClasses);
         printLog("【Create all table by reflect】");
     }
+
+    private static void reflectMethod(Database db, String methodName, boolean isExists, @NonNull Class<? extends AbstractDao<?, ?>>... daoClasses) {
+        if (daoClasses.length < 1) {
+            return;
+        }
+        try {
+            for (Class cls : daoClasses) {
+                Method method = cls.getDeclaredMethod(methodName, Database.class, boolean.class);
+                method.invoke(null, db, isExists);
+            }
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void restoreData(Database db, Class<? extends AbstractDao<?, ?>>... daoClasses) {
+        for (int i = 0; i < daoClasses.length; i++) {
+            DaoConfig daoConfig = new DaoConfig(db, daoClasses[i]);
+            String tableName = daoConfig.tablename;
+            String tempTableName = daoConfig.tablename.concat("_TEMP");
+
+            if (!isTableExists(db, true, tempTableName)) {
+                continue;
+            }
+
+            try {
+                // get all columns from tempTable, take careful to use the columns list
+                List<String> columns = getColumns(db, tempTableName);
+                ArrayList<String> properties = new ArrayList<>(columns.size());
+                for (int j = 0; j < daoConfig.properties.length; j++) {
+                    String columnName = daoConfig.properties[j].columnName;
+                    if (columns.contains(columnName)) {
+                        properties.add("`" + columnName + "`");
+                    }
+                }
+                if (properties.size() > 0) {
+                    final String columnSQL = TextUtils.join(",", properties);
+
+                    StringBuilder insertTableStringBuilder = new StringBuilder();
+                    insertTableStringBuilder.append("REPLACE INTO ").append(tableName).append(" (");
+                    insertTableStringBuilder.append(columnSQL);
+                    insertTableStringBuilder.append(") SELECT ");
+                    insertTableStringBuilder.append(columnSQL);
+                    insertTableStringBuilder.append(" FROM ").append(tempTableName).append(";");
+                    db.execSQL(insertTableStringBuilder.toString());
+                    printLog("【Restore data】 to " + tableName);
+                }
+                StringBuilder dropTableStringBuilder = new StringBuilder();
+                dropTableStringBuilder.append("DROP TABLE ").append(tempTableName);
+                db.execSQL(dropTableStringBuilder.toString());
+                printLog("【Drop temp table】" + tempTableName);
+            } catch (SQLException e) {
+                Log.e(TAG, "【Failed to restore data from temp table 】" + tempTableName, e);
+            }
+        }
+    }
+
+    private static List<String> getColumns(Database db, String tableName) {
+        List<String> columns = null;
+        Cursor cursor = null;
+        try {
+            cursor = db.rawQuery("SELECT * FROM " + tableName + " limit 0", null);
+            if (null != cursor && cursor.getColumnCount() > 0) {
+                columns = Arrays.asList(cursor.getColumnNames());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (cursor != null) cursor.close();
+            if (null == columns) columns = new ArrayList<>();
+        }
+        return columns;
+    }
+
+    private static void printLog(String info) {
+        if (DEBUG) {
+            Log.i(TAG, info);
+        }
+    }
 }
