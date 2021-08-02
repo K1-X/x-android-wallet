1f66314d85975363a7139f56f39439b3d720cae4
madisonpill <madisonpill@hotmail.com>
2021-08-03 06:30:01 +0800

    LocationManager.java added

---
+++ app/src/main/java/bd/com/bdwallet/util/LocationManager.java
@@ -0,0 +1,16 @@
+package bd.com.bdwallet.util;
+
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+
+import bd.com.bdwallet.app.BdApplication;
+
+public class LocationManager implements ILocation{
+    
+}
