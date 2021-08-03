1a6e9047e9575cbdd11103c30852c363c4141560
kolionsleep <kolionsleep@hotmail.com>
2021-08-04 07:50:01 +0800

    DbHelper.java

---
+++ walletdb/src/main/java/bd/com/walletdb/helper/DbHelper.java
@@ -0,0 +1,17 @@
+package bd.com.walletdb.helper;
+
+import android.content.Context;
+import android.util.Log;
+
+import org.greenrobot.greendao.database.Database;
+
+import bd.com.walletdb.config.Config;
+import bd.com.walletdb.greendao.DaoMaster;
+import bd.com.walletdb.greendao.DaoSession;
+import bd.com.walletdb.greendao.WalletEntityDao;
+
+
+
+public class DbHelper extends DaoMaster.OpenHelper {
+    
+}
