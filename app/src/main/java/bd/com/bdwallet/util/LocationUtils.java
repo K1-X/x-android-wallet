569b53fe48f262955ea87fd96d2c192b132b4f88
madisonpill <madisonpill@hotmail.com>
2021-08-05 07:50:01 +0800

    LocationUtils.java added

---
+++ app/src/main/java/bd/com/bdwallet/util/LocationUtils.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package bd.com.bdwallet.util;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.text.TextUtils;
+
+import com.amap.api.location.AMapLocation;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+public class LocationUtils {
+	
+}
