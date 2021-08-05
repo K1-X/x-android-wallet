6c01d42ae9b4ae2167c6900c0526463e8cc78ab4
madisonpill <madisonpill@hotmail.com>
2021-08-05 09:20:01 +0800

    getLocationStr

--- app/src/main/java/bd/com/bdwallet/util/LocationUtils.java
+++ app/src/main/java/bd/com/bdwallet/util/LocationUtils.java
@@ -31,4 +31,49 @@ public class LocationUtils {
 	
 	public final static String KEY_URL = "URL";
 	public final static String URL_H5LOCATION = "file:///android_asset/location.html";	
+
+    /**
+	 * 
+	 * @param location
+	 * @return
+	 */
+	public synchronized static String getLocationStr(AMapLocation location){
+		if(null == location){
+			return null;
+		}
+		StringBuffer sb = new StringBuffer();
+		//errCode0，，
+		if(location.getErrorCode() == 0){
+			sb.append("" + "\n");
+			sb.append(": " + location.getLocationType() + "\n");
+			sb.append("        : " + location.getLongitude() + "\n");
+			sb.append("        : " + location.getLatitude() + "\n");
+			sb.append("        : " + location.getAccuracy() + "" + "\n");
+			sb.append("    : " + location.getProvider() + "\n");
+			
+			sb.append("        : " + location.getSpeed() + "/" + "\n");
+			sb.append("        : " + location.getBearing() + "\n");
+			// 
+			sb.append("        : " + location.getSatellites() + "\n");
+			sb.append("        : " + location.getCountry() + "\n");
+			sb.append("            : " + location.getProvince() + "\n");
+			sb.append("            : " + location.getCity() + "\n");
+			sb.append(" : " + location.getCityCode() + "\n");
+			sb.append("            : " + location.getDistrict() + "\n");
+			sb.append("    : " + location.getAdCode() + "\n");
+			sb.append("        : " + location.getAddress() + "\n");
+			sb.append("    : " + location.getPoiName() + "\n");
+			//
+			sb.append(": " + formatUTC(location.getTime(), "yyyy-MM-dd HH:mm:ss") + "\n");
+		} else {
+			//
+			sb.append("" + "\n");
+			sb.append(":" + location.getErrorCode() + "\n");
+			sb.append(":" + location.getErrorInfo() + "\n");
+			sb.append(":" + location.getLocationDetail() + "\n");
+		}
+		//
+		sb.append(": " + formatUTC(System.currentTimeMillis(), "yyyy-MM-dd HH:mm:ss") + "\n");
+		return sb.toString();
+	}
 }
