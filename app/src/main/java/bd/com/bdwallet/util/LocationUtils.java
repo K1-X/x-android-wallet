5dff641de016e870f901fc667bcdbfce14346012
madisonpill <madisonpill@hotmail.com>
2021-08-05 09:00:01 +0800

    static attrs

--- app/src/main/java/bd/com/bdwallet/util/LocationUtils.java
+++ app/src/main/java/bd/com/bdwallet/util/LocationUtils.java
@@ -16,5 +16,19 @@ import java.text.SimpleDateFormat;
 import java.util.Locale;
 
 public class LocationUtils {
+    /**
+	 *  
+	 */
+	public final static int MSG_LOCATION_START = 0;
+	/**
+	 * 
+	 */
+	public final static int MSG_LOCATION_FINISH = 1;
+	/**
+	 * 
+	 */
+	public final static int MSG_LOCATION_STOP= 2;
 	
+	public final static String KEY_URL = "URL";
+	public final static String URL_H5LOCATION = "file:///android_asset/location.html";	
 }
