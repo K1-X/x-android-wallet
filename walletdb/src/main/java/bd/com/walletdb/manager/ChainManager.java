08d601465f4ec078deb79650671223bf0a1f70fd
kolionsleep <kolionsleep@hotmail.com>
2021-08-06 07:20:01 +0800

    ChainManager.java

---
+++ walletdb/src/main/java/bd/com/walletdb/manager/ChainManager.java
@@ -0,0 +1,11 @@
+package bd.com.walletdb.manager;
+
+import java.util.List;
+
+import bd.com.walletdb.action.ChainAction;
+import bd.com.walletdb.entity.ChainEntity;
+import bd.com.walletdb.greendao.ChainEntityDao;
+
+public class ChainManager {
+    
+}
