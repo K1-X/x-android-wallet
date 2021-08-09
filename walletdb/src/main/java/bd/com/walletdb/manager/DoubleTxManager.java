cad2d76f7fd3e775bb9eec42c927495c7c73a8be
kolionsleep <kolionsleep@hotmail.com>
2021-08-10 07:20:01 +0800

    DoubleTxManager.java

---
+++ walletdb/src/main/java/bd/com/walletdb/manager/DoubleTxManager.java
@@ -0,0 +1,13 @@
+package bd.com.walletdb.manager;
+
+import android.text.TextUtils;
+
+import java.util.List;
+
+import bd.com.walletdb.action.DouTxAction;
+import bd.com.walletdb.entity.DoubleTxEntity;
+import bd.com.walletdb.greendao.DoubleTxEntityDao;
+
+public class DoubleTxManager {
+    
+}
