e9ffd59b7806f9ee6dca03c01514b8ce146af2c4
madisonpill <madisonpill@hotmail.com>
2021-08-13 07:50:01 +0800

    toastNormal

--- app/src/main/java/bd/com/bdwallet/util/ToastUtil.java
+++ app/src/main/java/bd/com/bdwallet/util/ToastUtil.java
@@ -5,5 +5,14 @@ import android.widget.Toast;
 import bd.com.bdwallet.app.BdApplication;
 
 public class ToastUtil {
-    
+
+    private static final int TYPE_NORMAL = 0;
+    private static final int TYPE_ERROR = -1;
+    private static final int TYPE_SUCCESS = 1;
+    private static long currentToastTime;
+    private static String currentMessage;
+
+    public static void toastNormal(String message) {
+        toast(message, Toast.LENGTH_SHORT, TYPE_NORMAL);
+    }    
 }
