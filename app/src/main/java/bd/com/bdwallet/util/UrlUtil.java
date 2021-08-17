9f942436bc5014354e748dcd34eb44d9d6455fb1
madisonpill <madisonpill@hotmail.com>
2021-08-17 11:40:01 +0800

    UrlEntity parse

--- app/src/main/java/bd/com/bdwallet/util/UrlUtil.java
+++ app/src/main/java/bd/com/bdwallet/util/UrlUtil.java
@@ -17,4 +17,29 @@ public class UrlUtil {
         public Map<String, String> params;
     }    
 
+    public static UrlEntity parse(String url) {
+        UrlEntity entity = new UrlEntity();
+        if (url == null) {
+            return entity;
+        }
+        url = url.trim();
+        if (url.equals("")) {
+            return entity;
+        }
+        String[] urlParts = url.split("\\?");
+        entity.baseUrl = urlParts[0];
+        //
+        if (urlParts.length == 1) {
+            return entity;
+        }
+        //
+        String[] params = urlParts[1].split("&");
+        entity.params = new HashMap<>();
+        for (String param : params) {
+            String[] keyValue = param.split("=");
+            entity.params.put(keyValue[0], keyValue[1]);
+        }
+
+        return entity;
+    }
 }
