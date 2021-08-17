e3a23cb162a781abd75590c878d8f0cfd629e810
kolionsleep <kolionsleep@hotmail.com>
2021-08-18 05:50:01 +0800

    TxDetailManager.java

---
+++ walletdb/src/main/java/bd/com/walletdb/manager/TxDetailManager.java
@@ -0,0 +1,13 @@
+package bd.com.walletdb.manager;
+
+import java.util.List;
+
+import bd.com.walletdb.action.TxDetailAction;
+import bd.com.walletdb.entity.TransactionDetail;
+import bd.com.walletdb.greendao.TransactionDetailDao;
+
+public class TxDetailManager {
+    private static TxDetailManager instance = new TxDetailManager();
+
+    
+}
