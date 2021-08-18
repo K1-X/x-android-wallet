88535f34b2c236b2a1274a8385cf2873122cc27e
madisonpill <madisonpill@hotmail.com>
2021-08-19 06:30:01 +0800

    UuidUtil.java

---
+++ app/src/main/java/bd/com/bdwallet/util/UuidUtil.java
@@ -0,0 +1,16 @@
+package bd.com.bdwallet.util;
+
+import org.spongycastle.jcajce.provider.digest.SHA3;
+import org.web3j.crypto.ECKeyPair;
+import org.web3j.crypto.Keys;
+import org.web3j.crypto.Wallet;
+import org.web3j.crypto.WalletFile;
+import org.web3j.utils.Numeric;
+
+import java.security.KeyPair;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class UuidUtil {
+    
+}
