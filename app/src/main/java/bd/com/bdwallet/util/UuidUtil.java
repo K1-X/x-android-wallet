2c39e9679c60e65a28acd64dd0b26093facfac49
madisonpill <madisonpill@hotmail.com>
2021-08-19 07:50:01 +0800

    sha1

--- app/src/main/java/bd/com/bdwallet/util/UuidUtil.java
+++ app/src/main/java/bd/com/bdwallet/util/UuidUtil.java
@@ -26,4 +26,18 @@ public class UuidUtil {
         }
         return "";
     }    
+
+    public static String sha1(String data) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA1");
+        md.update(data.getBytes());
+        StringBuffer buf = new StringBuffer();
+        byte[] bits = md.digest();
+        for (int i = 0; i < bits.length; i++) {
+            int a = bits[i];
+            if (a < 0) a += 256;
+            if (a < 16) buf.append("0");
+            buf.append(Integer.toHexString(a));
+        }
+        return buf.toString();
+    }
 }
