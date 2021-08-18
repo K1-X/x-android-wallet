6984ddbf61285d5d1d98e2ac995a25ee93634daa
kolionsleep <kolionsleep@hotmail.com>
2021-08-18 09:20:01 +0800

    getWalletEntity

--- walletdb/src/main/java/bd/com/walletdb/manager/WalletDBManager.java
+++ walletdb/src/main/java/bd/com/walletdb/manager/WalletDBManager.java
@@ -10,5 +10,17 @@ import bd.com.walletdb.greendao.WalletEntityDao;
 public class WalletDBManager {
     private static WalletDBManager manager = new WalletDBManager();
 
-    
+    public static WalletDBManager getManager() {
+        return manager;
+    }
+
+    public WalletEntity getWalletEntity(String address) {
+        WalletAction action = new WalletAction();
+        List<WalletEntity> walletEntities = action.eq(WalletEntityDao.Properties.Address, address).queryAnd();
+        if (walletEntities != null && walletEntities.size() > 0) {
+            return walletEntities.get(0);
+        } else {
+            return null;
+        }
+    }    
 }
