b55ea8b2053d79bbbc59b26d07cad48a1c93696f
kolionsleep <kolionsleep@hotmail.com>
2021-08-18 09:00:01 +0800

    WalletDBManager.java

---
+++ walletdb/src/main/java/bd/com/walletdb/manager/WalletDBManager.java
@@ -0,0 +1,14 @@
+package bd.com.walletdb.manager;
+
+import java.util.List;
+
+import bd.com.walletdb.action.WalletAction;
+import bd.com.walletdb.entity.WalletEntity;
+import bd.com.walletdb.greendao.WalletEntityDao;
+
+
+public class WalletDBManager {
+    private static WalletDBManager manager = new WalletDBManager();
+
+    
+}
