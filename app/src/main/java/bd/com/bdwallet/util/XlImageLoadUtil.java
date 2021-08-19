2c3bb2ce01872e0ac15315ab2fa887189bab8947
madisonpill <madisonpill@hotmail.com>
2021-08-19 09:00:01 +0800

    XlImageLoadUtil

---
+++ app/src/main/java/bd/com/bdwallet/util/XlImageLoadUtil.java
@@ -0,0 +1,24 @@
+package bd.com.bdwallet.util;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
+
+import bd.com.bdwallet.app.BdApplication;
+
+public class XlImageLoadUtil {
+    
+}
