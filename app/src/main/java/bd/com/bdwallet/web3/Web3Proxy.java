5437439c45c5ed4e529fcf6795016490f2a03e92
madisonpill <madisonpill@hotmail.com>
2021-08-23 07:20:01 +0800

    class Web3Proxy

--- app/src/main/java/bd/com/bdwallet/web3/Web3Proxy.java
+++ app/src/main/java/bd/com/bdwallet/web3/Web3Proxy.java
@@ -26,5 +26,19 @@ import bd.com.walletdb.manager.WalletDBManager;
 
 
 public class Web3Proxy {
-   
+
+    //private String contractaddress = "0x0f46a24b42923aae949cd82b78b90b21e990cbad";
+    private String contractaddress = "0x0f46a24b42923aae949cd82b78b90b21e990cbad";
+    public static final String SCT_CONTRACT_ADDRESS = "0xsctcontractaddress";
+    //    public static final BigInteger GAS_PRICE = BigInteger.valueOf(10000000000L);
+//    public static BigInteger GAS_LIMIT = BigInteger.valueOf(2000000);
+    public static BigInteger GAS_PRICE = BigInteger.valueOf(22_000_000_000L);
+    //public static  BigInteger GAS_LIMIT = BigInteger.valueOf(2200000);
+    public static BigInteger GAS_LIMIT = BigInteger.valueOf(60000);
+    public static BigInteger DEPLOY_GAS_LIMIT = BigInteger.valueOf(2000000);
+    public static BigInteger PACK_SOSO_GAS_LIMIT = BigInteger.valueOf(1_000_010_000L);
+    private static Web3Proxy web3Proxy = new Web3Proxy();
+
+    private Web3Proxy() {
+    }   
 }
