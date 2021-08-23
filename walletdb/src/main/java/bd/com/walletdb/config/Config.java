b0c03c21e47108bed663e86614cd9293a3e2ea86
kolionsleep <kolionsleep@hotmail.com>
2021-08-24 07:20:01 +0800

    Config.java

---
+++ walletdb/src/main/java/bd/com/walletdb/config/Config.java
@@ -0,0 +1,7 @@
+package bd.com.walletdb.config;
+
+public class Config {
+    public static final String BD_WALLET = "bd_wallet";  // 
+    public static final String BD_TRANSACTION = "bd_transaction";  // 
+    public static final String BD_CONTACT = "bd_contact";  // 
+}
