a0203d9c355545ee733f0198ee7f954db17570f5
kolionsleep <kolionsleep@hotmail.com>
2021-08-24 09:00:01 +0800

    construct DoubleTxEntity

--- walletdb/src/main/java/bd/com/walletdb/DoubleTxEntity.java
+++ walletdb/src/main/java/bd/com/walletdb/DoubleTxEntity.java
@@ -5,5 +5,16 @@ import org.greenrobot.greendao.annotation.Id;
 import org.greenrobot.greendao.annotation.Generated;
 @Entity
 public class DoubleTxEntity {
-    
+
+    @Id
+    private String to;
+    private String from;
+    private long time;
+
+    @Generated(hash = 510195246)
+    public DoubleTxEntity(String to, String from, long time) {
+        this.to = to;
+        this.from = from;
+        this.time = time;
+    }    
 }
