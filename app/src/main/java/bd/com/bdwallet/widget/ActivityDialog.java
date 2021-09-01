7c953747e5a0009cab6557d63b714a59beb5c25b
madisonpill <madisonpill@hotmail.com>
2021-09-02 07:50:01 +0800

    ActivityDialog.java

---
+++ app/src/main/java/bd/com/bdwallet/widget/ActivityDialog.java
@@ -0,0 +1,17 @@
+package bd.com.bdwallet.widget;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.StyleRes;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import bd.com.bdwallet.R;
+
+
+public class ActivityDialog extends Dialog {
+    
+
+}
