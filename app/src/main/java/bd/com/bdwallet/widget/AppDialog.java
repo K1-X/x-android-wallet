9a3c5cf1a341cabb1c91442db24edc4f4483dee2
madisonpill <madisonpill@hotmail.com>
2021-09-06 05:50:01 +0800

    AppDialog.java

---
+++ app/src/main/java/bd/com/bdwallet/widget/AppDialog.java
@@ -0,0 +1,32 @@
+package bd.com.bdwallet.widget;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.cunoraz.gifview.library.GifView;
+
+import java.util.concurrent.TimeUnit;
+
+
+import bd.com.bdwallet.R;
+import io.reactivex.Observable;
+import io.reactivex.functions.Action;
+
+
+public class AppDialog extends Dialog {
+
+    
+
+}
