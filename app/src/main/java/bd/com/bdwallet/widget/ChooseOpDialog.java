97fd4663556a195253a7c6cda25c7d4411ff8112
madisonpill <madisonpill@hotmail.com>
2021-09-07 07:20:01 +0800

    ChooseOpDialog.java

---
+++ app/src/main/java/bd/com/bdwallet/widget/ChooseOpDialog.java
@@ -0,0 +1,21 @@
+package bd.com.bdwallet.widget;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.StyleRes;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import bd.com.bdwallet.R;
+
+
+public class ChooseOpDialog extends Dialog {
+    
+
+
+}
