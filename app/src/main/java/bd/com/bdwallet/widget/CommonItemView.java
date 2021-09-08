ab48071e454029eb325c7cfb6c547b6692a7ee64
madisonpill <madisonpill@hotmail.com>
2021-09-09 05:50:01 +0800

    CommonItemView.java

---
+++ app/src/main/java/bd/com/bdwallet/widget/CommonItemView.java
@@ -0,0 +1,19 @@
+package bd.com.bdwallet.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.StringRes;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import bd.com.bdwallet.R;
+
+
+public class CommonItemView extends RelativeLayout {
+    
+}
