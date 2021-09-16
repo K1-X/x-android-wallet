b0ebd30c450bad4c1b83a62d9b2f3013a435e007
madisonpill <madisonpill@hotmail.com>
2021-09-17 06:30:01 +0800

    CustomRefreshHeader.java

---
+++ app/src/main/java/bd/com/bdwallet/widget/CustomRefreshHeader.java
@@ -0,0 +1,27 @@
+package bd.com.bdwallet.widget;
+
+import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
+import android.support.annotation.NonNull;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+import com.scwang.smartrefresh.layout.api.RefreshHeader;
+import com.scwang.smartrefresh.layout.api.RefreshKernel;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.constant.RefreshState;
+import com.scwang.smartrefresh.layout.constant.SpinnerStyle;
+
+import bd.com.bdwallet.R;
+
+public class CustomRefreshHeader extends LinearLayout implements RefreshHeader {
+    
+
+}
