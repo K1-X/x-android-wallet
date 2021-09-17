38db28d9f450518cdc394d3afc13134cfcfba761
kolionsleep <kolionsleep@hotmail.com>
2021-09-18 07:50:01 +0800

    SosoLoadingDialog.java

---
+++ app/src/main/java/bd/com/bdwallet/widget/SosoLoadingDialog.java
@@ -0,0 +1,25 @@
+package bd.com.bdwallet.widget;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.gif.GifDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+import com.cunoraz.gifview.library.GifView;
+
+import bd.com.appcore.R;
+
+public class SosoLoadingDialog {
+    
+
+}
