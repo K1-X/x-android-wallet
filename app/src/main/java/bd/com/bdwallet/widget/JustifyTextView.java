c3b57a11ac9e1cd0d1824fb9c3cdc040eee0337f
madisonpill <madisonpill@hotmail.com>
2021-09-18 06:30:01 +0800

    JustfyTestView.java

---
+++ app/src/main/java/bd/com/bdwallet/widget/JustifyTextView.java
@@ -0,0 +1,14 @@
+package bd.com.bdwallet.widget;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class JustifyTextView extends android.support.v7.widget.AppCompatTextView {
+    
+}
