5c1af85dd9d74d0e7388a04a3cf87214702fb42b
kolionsleep <kolionsleep@hotmail.com>
2021-09-18 05:20:01 +0800

    SelectChainDialog.java

---
+++ app/src/main/java/bd/com/bdwallet/widget/SelectChainDialog.java
@@ -0,0 +1,28 @@
+package bd.com.bdwallet.widget;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.StyleRes;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import bd.com.bdwallet.R;
+import bd.com.bdwallet.module.common.TextWatcherProxy;
+import bd.com.bdwallet.module.common.lvadapter.BaseHolder;
+import bd.com.bdwallet.module.common.lvadapter.SelectChainHolder;
+import bd.com.bdwallet.module.common.lvadapter.SuperAdapter;
+import bd.com.walletdb.entity.ChainEntity;
+
+public class SelectChainDialog extends Dialog {
+    
+}
