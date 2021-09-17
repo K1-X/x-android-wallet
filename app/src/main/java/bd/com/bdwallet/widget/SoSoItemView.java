14885f34f675ab214a5b45e5aee5d0d63db32e60
kolionsleep <kolionsleep@hotmail.com>
2021-09-18 07:20:01 +0800

    SoSoItemView.java

---
+++ app/src/main/java/bd/com/bdwallet/widget/SoSoItemView.java
@@ -0,0 +1,24 @@
+package bd.com.bdwallet.widget;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import bd.com.appcore.ui.adapter.CommonAdapter;
+import bd.com.appcore.ui.adapter.base.ViewHolder;
+import bd.com.bdwallet.R;
+import bd.com.bdwallet.module.transaction.model.domian.ArchivesBean;
+import bd.com.bdwallet.module.transaction.model.domian.SoSoBean;
+import bd.com.bdwallet.module.transaction.model.domian.SoSoInnerItemBean;
+
+public class SoSoItemView extends LinearLayout {
+    
+}
