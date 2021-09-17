98178db6723d5e6f45f83c106bdf3f5c030f7755
kolionsleep <kolionsleep@hotmail.com>
2021-09-18 07:50:01 +0800

    PriveteKeyDialog.java

---
+++ app/src/main/java/bd/com/bdwallet/widget/PriveteKeyDialog.java
@@ -0,0 +1,18 @@
+package bd.com.bdwallet.widget;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.StyleRes;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import bd.com.bdwallet.R;
+import bd.com.bdwallet.util.ResourceUtil;
+
+public class PriveteKeyDialog extends Dialog {
+    
+
+}
