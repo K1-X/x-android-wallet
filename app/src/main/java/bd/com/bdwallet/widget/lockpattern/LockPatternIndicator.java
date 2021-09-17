959c41b61e3eac0517f256b673ae44054e5e3aeb
madisonpill <madisonpill@hotmail.com>
2021-09-18 07:20:01 +0800

    construct method

--- app/src/main/java/bd/com/bdwallet/widget/lockpattern/LockPatternIndicator.java
+++ app/src/main/java/bd/com/bdwallet/widget/lockpattern/LockPatternIndicator.java
@@ -12,5 +12,26 @@ import bd.com.bdwallet.R;
 
 
 public class LockPatternIndicator extends View {
-    
+
+    private int width, height;
+    private int cellBoxWidth, cellBoxHeight;
+    private int radius;
+    private int offset = 2;
+    private Paint defaultPaint, selectPaint;
+    private IndicatorCell[][] mIndicatorCells = new IndicatorCell[3][3];
+
+    private static final String TAG = "LockPatternIndicator";
+
+    public LockPatternIndicator(Context context) {
+        this(context, null);
+    }
+
+    public LockPatternIndicator(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public LockPatternIndicator(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.init();
+    }    
 }
