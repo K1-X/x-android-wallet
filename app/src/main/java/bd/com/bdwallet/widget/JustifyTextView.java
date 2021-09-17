182a48d69dc5d0d5f052ab588e2f80014e27be0f
madisonpill <madisonpill@hotmail.com>
2021-09-18 07:20:01 +0800

    JustifyTextView

--- app/src/main/java/bd/com/bdwallet/widget/JustifyTextView.java
+++ app/src/main/java/bd/com/bdwallet/widget/JustifyTextView.java
@@ -10,5 +10,12 @@ import android.util.AttributeSet;
 import android.widget.TextView;
 
 public class JustifyTextView extends android.support.v7.widget.AppCompatTextView {
-    
+
+    private int mLineY;
+    private int mViewWidth;
+    public static final String TWO_CHINESE_BLANK = "  ";
+
+    public JustifyTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }    
 }
