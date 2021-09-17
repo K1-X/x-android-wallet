ffa876a658abd5c122d8676d9ba2c1b1d3a49b55
madisonpill <madisonpill@hotmail.com>
2021-09-18 06:30:01 +0800

    LockPatternIndicator.java

---
+++ app/src/main/java/bd/com/bdwallet/widget/lockpattern/LockPatternIndicator.java
@@ -0,0 +1,16 @@
+package bd.com.bdwallet.widget.lockpattern;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.util.List;
+
+import bd.com.bdwallet.R;
+
+
+public class LockPatternIndicator extends View {
+    
+}
