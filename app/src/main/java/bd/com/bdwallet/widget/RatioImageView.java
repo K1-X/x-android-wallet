141fa2bccefd1c14b66d3a1c6e4d02eee7c5d36c
madisonpill <madisonpill@hotmail.com>
2021-09-18 07:50:01 +0800

    RatioImageView.java

---
+++ app/src/main/java/bd/com/bdwallet/widget/RatioImageView.java
@@ -0,0 +1,13 @@
+package bd.com.bdwallet.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+import bd.com.bdwallet.R;
+
+
+public class RatioImageView extends ImageView {
+    
+}
