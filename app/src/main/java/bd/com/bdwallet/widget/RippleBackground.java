c5c3f961cdf707cf5d06f28c569074eb70225981
madisonpill <madisonpill@hotmail.com>
2021-09-18 05:50:01 +0800

    RippleBackground.java

---
+++ app/src/main/java/bd/com/bdwallet/widget/RippleBackground.java
@@ -0,0 +1,24 @@
+package bd.com.bdwallet.widget;
+
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.RelativeLayout;
+
+
+import java.util.ArrayList;
+
+import bd.com.bdwallet.R;
+
+
+public class RippleBackground extends RelativeLayout {
+
+    
+}
