0fb68f14af62788c745534f64affb3bbb6e5c0aa
madisonpill <madisonpill@hotmail.com>
2021-09-18 07:50:01 +0800

    add:changeSize

--- app/src/main/java/bd/com/bdwallet/widget/lockpattern/LockPatternUtil.java
+++ app/src/main/java/bd/com/bdwallet/widget/lockpattern/LockPatternUtil.java
@@ -10,5 +10,21 @@ import java.util.List;
 
 public class LockPatternUtil {
 
+    @Deprecated
+    public static int changeSize(Context context, String value) {
+        if (value.contains("dip")) {
+            float dip = Float.valueOf(value.substring(0, value.indexOf("dip")));
+            return LockPatternUtil.dip2px(context, dip);
+        } else if (value.contains("px")) {
+            float px = Float.valueOf(value.substring(0, value.indexOf("px")));
+            return (int) px;
+        } else if (value.contains("@")) {
+            float px = context.getResources().getDimension(Integer.valueOf(value.replace("@", "")));
+            return (int) px;
+        } else {
+            throw new IllegalArgumentException("can not use wrap_content " +
+                "or match_parent or fill_parent or others' illegal parameter");
+        }
+    }
     
 }
