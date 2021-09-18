d089f2818329c07a7d199153a078702137d01416
madisonpill <madisonpill@hotmail.com>
2021-09-18 09:00:01 +0800

    onDraw

--- app/src/main/java/bd/com/bdwallet/widget/JustifyTextView.java
+++ app/src/main/java/bd/com/bdwallet/widget/JustifyTextView.java
@@ -23,4 +23,44 @@ public class JustifyTextView extends android.support.v7.widget.AppCompatTextView
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
     }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        TextPaint paint = getPaint();
+        paint.setColor(getCurrentTextColor());
+        paint.drawableState = getDrawableState();
+        mViewWidth = getMeasuredWidth();
+        String text = getText().toString();
+        mLineY = 0;
+        mLineY += getTextSize();
+        Layout layout = getLayout();
+
+        // layout.getLayout()4.4.3NullPointerException
+        if (layout == null) {
+            return;
+        }
+
+        Paint.FontMetrics fm = paint.getFontMetrics();
+
+        int textHeight = (int) (Math.ceil(fm.descent - fm.ascent));
+        textHeight = (int) (textHeight * layout.getSpacingMultiplier() + layout.getSpacingAdd());
+        //
+        for (int i = 0; i < layout.getLineCount(); i++) {
+            int lineStart = layout.getLineStart(i);
+            int lineEnd = layout.getLineEnd(i);
+            float width = StaticLayout.getDesiredWidth(text, lineStart, lineEnd, getPaint());
+            String line = text.substring(lineStart, lineEnd);
+
+            if (i < layout.getLineCount() - 1) {
+                if (needScale(line)) {
+                    drawScaledText(canvas, lineStart, line, width);
+                } else {
+                    canvas.drawText(line, 0, mLineY, paint);
+                }
+            } else {
+                canvas.drawText(line, 0, mLineY, paint);
+            }
+            mLineY += textHeight;
+        }
+    }
 }
