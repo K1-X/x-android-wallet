b04087c44867d48634939e2ad9802983ee996c6b
madisonpill <madisonpill@hotmail.com>
2021-09-18 10:10:01 +0800

    init method

--- app/src/main/java/bd/com/bdwallet/widget/RatioImageView.java
+++ app/src/main/java/bd/com/bdwallet/widget/RatioImageView.java
@@ -26,4 +26,16 @@ public class RatioImageView extends ImageView {
         super(context, attrs, defStyleAttr);
         this.init(attrs);
     }
+
+    private void init(AttributeSet attrs){
+        TypedArray a = getContext().obtainStyledAttributes(attrs,
+                R.styleable.RatioImageView);
+        h2wratio = a.getFloat(
+                R.styleable.RatioImageView_height_according_to_width_ratio, h2wratio);
+        w2hratio = a.getFloat(
+                R.styleable.RatioImageView_width_according_to_height_ratio, w2hratio);
+
+
+        a.recycle();
+    }
 }
