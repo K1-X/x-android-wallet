d364ae05913fdd5ff2c86ad3b4ebbc7a5b9a7a49
kolionsleep <kolionsleep@hotmail.com>
2021-09-18 10:10:01 +0800

    ViewPagerSlide.java

---
+++ app/src/main/java/bd/com/bdwallet/widget/ViewPagerSlide.java
@@ -0,0 +1,13 @@
+package bd.com.bdwallet.widget;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class ViewPagerSlide extends ViewPager {
+    //
+    private boolean isSlide = false;
+
+    
+}
