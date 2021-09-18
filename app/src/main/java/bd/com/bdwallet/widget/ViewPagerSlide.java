fc2372085cd54c93885f8506cc3c3e7012a135f7
kolionsleep <kolionsleep@hotmail.com>
2021-09-18 11:40:01 +0800

    onInterceptTouchEvent

--- app/src/main/java/bd/com/bdwallet/widget/ViewPagerSlide.java
+++ app/src/main/java/bd/com/bdwallet/widget/ViewPagerSlide.java
@@ -9,5 +9,24 @@ public class ViewPagerSlide extends ViewPager {
     //
     private boolean isSlide = false;
 
+    public ViewPagerSlide(Context context) {
+        super(context);
+    }
+
+    public ViewPagerSlide(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if (isSlide){
+            return super.onInterceptTouchEvent(ev);
+        }
+        return isSlide;
+    }
+
+    public void setSlide(boolean slide) {
+        isSlide = slide;
+    }
     
 }
