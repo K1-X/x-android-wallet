8d275ecd4a982451afc7d46309c260b7f8125522
madisonpill <madisonpill@hotmail.com>
2021-09-18 09:20:01 +0800

    construct RatioImageView

--- app/src/main/java/bd/com/bdwallet/widget/RatioImageView.java
+++ app/src/main/java/bd/com/bdwallet/widget/RatioImageView.java
@@ -16,4 +16,14 @@ public class RatioImageView extends ImageView {
     public RatioImageView(Context ctx){
         super(ctx);
     }    
+
+    public RatioImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.init(attrs);
+    }
+
+    public RatioImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.init(attrs);
+    }
 }
