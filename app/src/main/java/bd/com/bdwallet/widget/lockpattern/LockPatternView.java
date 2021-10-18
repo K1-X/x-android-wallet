47b367ad671896b5f6bc3ff2a481b508615767dc
madisonpill <madisonpill@hotmail.com>
2021-10-19 06:30:01 +0800

    LockPatternView.java

---
+++ app/src/main/java/bd/com/bdwallet/widget/lockpattern/LockPatternView.java
@@ -0,0 +1,23 @@
+package bd.com.bdwallet.widget.lockpattern;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.HapticFeedbackConstants;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import bd.com.bdwallet.R;
+
+
+public class LockPatternView extends View {
+
+    
+}
