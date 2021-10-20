b5a62f356e2ae116c45ef699f34be01499d0149f
madisonpill <madisonpill@hotmail.com>
2021-10-21 07:50:01 +0800

    add:dip2px

--- app/src/main/java/bd/com/bdwallet/widget/marqueeview/Utils.java
+++ app/src/main/java/bd/com/bdwallet/widget/marqueeview/Utils.java
@@ -25,4 +25,10 @@ public class Utils {
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (pxValue / scale + 0.5f);
     }
+
+    // dipdppx
+    public static int dip2px(Context context, float dipValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dipValue * scale + 0.5f);
+    }
 }
