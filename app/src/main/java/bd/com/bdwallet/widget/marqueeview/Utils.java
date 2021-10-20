dedb9c63e26006f87cd8c302d5c6debaffca802a
madisonpill <madisonpill@hotmail.com>
2021-10-21 06:30:01 +0800

    isEmpty and isNotEmpty

--- app/src/main/java/bd/com/bdwallet/widget/marqueeview/Utils.java
+++ app/src/main/java/bd/com/bdwallet/widget/marqueeview/Utils.java
@@ -8,6 +8,16 @@ import java.util.List;
 
 public class Utils {
 
-    
+
+    public static <T> boolean notEmpty(List<T> list) {
+        return !isEmpty(list);
+    }
+
+    public static <T> boolean isEmpty(List<T> list) {
+        if (list == null || list.size() == 0) {
+            return true;
+        }
+        return false;
+    }    
 
 }
