b0f76d857d2a5da8b4550c1a95bf6f5770556cab
madisonpill <madisonpill@hotmail.com>
2021-10-21 05:50:01 +0800

    Utils.java

---
+++ app/src/main/java/bd/com/bdwallet/widget/marqueeview/Utils.java
@@ -0,0 +1,13 @@
+package bd.com.bdwallet.widget.marqueeview;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.DisplayMetrics;
+
+import java.util.List;
+
+public class Utils {
+
+    
+
+}
