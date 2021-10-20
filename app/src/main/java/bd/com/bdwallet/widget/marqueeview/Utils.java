2e38f33ebe088f28f3244a6f9d04241b36b0bab6
madisonpill <madisonpill@hotmail.com>
2021-10-21 07:20:01 +0800

    px2dip

--- app/src/main/java/bd/com/bdwallet/widget/marqueeview/Utils.java
+++ app/src/main/java/bd/com/bdwallet/widget/marqueeview/Utils.java
@@ -20,4 +20,9 @@ public class Utils {
         return false;
     }    
 
+    // pxdipdp
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
 }
