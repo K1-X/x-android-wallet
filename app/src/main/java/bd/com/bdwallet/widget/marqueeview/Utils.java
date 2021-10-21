8db6a39581ccf179f8986258a4463a3b940f51b9
madisonpill <madisonpill@hotmail.com>
2021-10-21 10:10:01 +0800

    isChinese

--- app/src/main/java/bd/com/bdwallet/widget/marqueeview/Utils.java
+++ app/src/main/java/bd/com/bdwallet/widget/marqueeview/Utils.java
@@ -57,4 +57,16 @@ public class Utils {
         context.getWindowManager().getDefaultDisplay().getMetrics(metric);
         return metric.heightPixels;
     }
+
+    // Unicode
+    private static boolean isChinese(char c) {
+        Character.UnicodeBlock ub = Character.UnicodeBlock.of(c);
+        if (ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS || ub == Character.UnicodeBlock.CJK_COMPATIBILITY_IDEOGRAPHS
+                || ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_A || ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_B
+                || ub == Character.UnicodeBlock.CJK_SYMBOLS_AND_PUNCTUATION || ub == Character.UnicodeBlock.HALFWIDTH_AND_FULLWIDTH_FORMS
+                || ub == Character.UnicodeBlock.GENERAL_PUNCTUATION) {
+            return true;
+        }
+        return false;
+    }
 }
