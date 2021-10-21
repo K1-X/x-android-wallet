918988de952ccd47448b37f6fbddef10d1d9e9c1
kolionsleep <kolionsleep@hotmail.com>
2021-10-22 07:50:01 +0800

    WalletItemView

---
+++ app/src/main/java/bd/com/bdwallet/widget/WalletItemView.java
@@ -0,0 +1,22 @@
+package bd.com.bdwallet.widget;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import bd.com.appcore.ui.view.CircleImageView;
+import bd.com.appcore.util.AppSettings;
+import bd.com.bdwallet.R;
+import bd.com.bdwallet.module.wallet.bus.WalletChangeEvent;
+import bd.com.bdwallet.util.ResourceUtil;
+import bd.com.walletdb.entity.WalletEntity;
+import de.greenrobot.event.EventBus;
+
+public class WalletItemView extends RelativeLayout {
+    
+
+}
