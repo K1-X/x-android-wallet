97881d0d61e604bff984adfac96ce29285193202
madisonpill <madisonpill@hotmail.com>
2021-10-21 09:00:01 +0800

    add:sp2px

--- app/src/main/java/bd/com/bdwallet/widget/marqueeview/Utils.java
+++ app/src/main/java/bd/com/bdwallet/widget/marqueeview/Utils.java
@@ -31,4 +31,16 @@ public class Utils {
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (dipValue * scale + 0.5f);
     }
+
+    // pxsp
+    public static int px2sp(Context context, float pxValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+    // sppx
+    public static int sp2px(Context context, float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
 }
