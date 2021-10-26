4f8b86a8fc2d93f40ac564c07a7537f180837483
madisonpill <madisonpill@hotmail.com>
2021-10-26 09:20:01 +0800

    MarqueTextView

---
+++ app/src/main/java/bd/com/bdwallet/widget/marqueeview/MarqueTextView.java
@@ -0,0 +1,10 @@
+package bd.com.bdwallet.widget.marqueeview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class MarqueTextView extends TextView {
+    
+
+}
