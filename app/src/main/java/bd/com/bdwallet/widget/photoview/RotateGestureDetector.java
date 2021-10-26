efc2b3948a790bfaa0c5ffa2115452d1cbbd8b85
kolionsleep <kolionsleep@hotmail.com>
2021-10-27 07:50:01 +0800

    mPrevSlope

--- app/src/main/java/bd/com/bdwallet/widget/photoview/RotateGestureDetector.java
+++ app/src/main/java/bd/com/bdwallet/widget/photoview/RotateGestureDetector.java
@@ -5,5 +5,16 @@ import android.view.MotionEvent;
 
 public class RotateGestureDetector {
 
+    private static final int MAX_DEGREES_STEP = 120;
+
+    private OnRotateListener mListener;
+
+    private float mPrevSlope;
+    private float mCurrSlope;
+
+    private float x1;
+    private float y1;
+    private float x2;
+    private float y2;
     
 }
