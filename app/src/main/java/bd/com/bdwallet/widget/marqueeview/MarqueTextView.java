32b9371c513a98ac49151bcf92281d55e84c77d1
madisonpill <madisonpill@hotmail.com>
2021-10-26 10:10:01 +0800

    construct method MarqueTextView

--- app/src/main/java/bd/com/bdwallet/widget/marqueeview/MarqueTextView.java
+++ app/src/main/java/bd/com/bdwallet/widget/marqueeview/MarqueTextView.java
@@ -5,6 +5,17 @@ import android.util.AttributeSet;
 import android.widget.TextView;
 
 public class MarqueTextView extends TextView {
-    
+
+    public MarqueTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public MarqueTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MarqueTextView(Context context) {
+        super(context);
+    }    
 
 }
