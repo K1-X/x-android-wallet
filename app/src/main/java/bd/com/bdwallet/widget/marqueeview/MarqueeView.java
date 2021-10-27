6b7c99fafbc72c2ecbbdb9afe8e963470a2957c5
madisonpill <madisonpill@hotmail.com>
2021-10-28 07:50:01 +0800

    add:startWithText

--- app/src/main/java/bd/com/bdwallet/widget/marqueeview/MarqueeView.java
+++ app/src/main/java/bd/com/bdwallet/widget/marqueeview/MarqueeView.java
@@ -119,4 +119,30 @@ public class MarqueeView<T> extends ViewFlipper {
         typedArray.recycle();
         setFlipInterval(interval);
     }    
+
+    public void startWithText(String message) {
+        startWithText(message, inAnimResId, outAnimResId);
+    }
+
+    /**
+     * ，
+     *
+     * @param message      
+     * @param inAnimResId  resID
+     * @param outAnimResID resID
+     */
+    public void startWithText(final String message, final @AnimRes int inAnimResId, final @AnimRes int outAnimResID) {
+        if (TextUtils.isEmpty(message)) return;
+        getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                } else {
+                    getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                }
+                startWithFixedWidth(message, inAnimResId, outAnimResID);
+            }
+        });
+    }
 }
