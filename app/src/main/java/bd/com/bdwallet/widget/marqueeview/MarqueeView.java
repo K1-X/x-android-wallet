1a7b100badc145993a6da7c98a3b971be38f9d3c
madisonpill <madisonpill@hotmail.com>
2021-10-28 06:30:01 +0800

    construct MarqueeView

--- app/src/main/java/bd/com/bdwallet/widget/marqueeview/MarqueeView.java
+++ app/src/main/java/bd/com/bdwallet/widget/marqueeview/MarqueeView.java
@@ -24,5 +24,42 @@ import bd.com.bdwallet.R;
 
 public class MarqueeView<T> extends ViewFlipper {
 
+    private int interval = 3000;
+    private boolean hasSetAnimDuration = false;
+    private int animDuration = 1000;
+    private int textSize = 14;
+    private int textColor = 0xff000000;
+    private boolean singleLine = false;
+
+    private int gravity = Gravity.LEFT | Gravity.CENTER_VERTICAL;
+    private static final int GRAVITY_LEFT = 0;
+    private static final int GRAVITY_CENTER = 1;
+    private static final int GRAVITY_RIGHT = 2;
+
+    private int direction = DIRECTION_BOTTOM_TO_TOP;
+    private static final int DIRECTION_BOTTOM_TO_TOP = 0;
+    private static final int DIRECTION_TOP_TO_BOTTOM = 1;
+    private static final int DIRECTION_RIGHT_TO_LEFT = 2;
+    private static final int DIRECTION_LEFT_TO_RIGHT = 3;
+
+    private Typeface typeface;
+
+    @AnimRes
+    private int inAnimResId = R.anim.anim_bottom_in;
+    @AnimRes
+    private int outAnimResId = R.anim.anim_top_out;
+
+    private int position;
+    private List<T> messages = new ArrayList<>();
+    private OnItemClickListener onItemClickListener;
+
+    public MarqueeView(Context context) {
+        this(context, null);
+    }
+
+    public MarqueeView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs, 0);
+    }
     
 }
