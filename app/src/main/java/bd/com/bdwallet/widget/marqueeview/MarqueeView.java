d50545c09b07a7e9f938213467370c794056e5bb
madisonpill <madisonpill@hotmail.com>
2021-10-28 05:50:01 +0800

    MarqueeView.java

---
+++ app/src/main/java/bd/com/bdwallet/widget/marqueeview/MarqueeView.java
@@ -0,0 +1,28 @@
+package bd.com.bdwallet.widget.marqueeview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.support.annotation.AnimRes;
+import android.support.annotation.FontRes;
+import android.support.v4.content.res.ResourcesCompat;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+import android.widget.ViewFlipper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import bd.com.bdwallet.R;
+
+public class MarqueeView<T> extends ViewFlipper {
+
+    
+}
