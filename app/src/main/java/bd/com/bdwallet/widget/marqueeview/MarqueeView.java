b4470378f15dc5115dbaeb88165b9d393aa0f809
madisonpill <madisonpill@hotmail.com>
2021-10-28 10:10:01 +0800

    create new view

--- app/src/main/java/bd/com/bdwallet/widget/marqueeview/MarqueeView.java
+++ app/src/main/java/bd/com/bdwallet/widget/marqueeview/MarqueeView.java
@@ -190,4 +190,96 @@ public class MarqueeView<T> extends ViewFlipper {
         setMessages(messages);
         postStart(inAnimResId, outAnimResID);
     }
+
+    private void postStart(final @AnimRes int inAnimResId, final @AnimRes int outAnimResID) {
+        post(new Runnable() {
+            @Override
+            public void run() {
+                start(inAnimResId, outAnimResID);
+            }
+        });
+    }
+
+    private boolean isAnimStart = false;
+
+    private void start(final @AnimRes int inAnimResId, final @AnimRes int outAnimResID) {
+        removeAllViews();
+        clearAnimation();
+        // 
+        if (messages == null || messages.isEmpty()) {
+            throw new RuntimeException("The messages cannot be empty!");
+        }
+        position = 0;
+        addView(createTextView(messages.get(position)));
+
+        if (messages.size() > 1) {
+            setInAndOutAnimation(inAnimResId, outAnimResID);
+            startFlipping();
+        }
+
+        if (getInAnimation() != null) {
+            getInAnimation().setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+                    if (isAnimStart) {
+                        animation.cancel();
+                    }
+                    isAnimStart = true;
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    position++;
+                    if (position >= messages.size()) {
+                        position = 0;
+                    }
+                    View view = createTextView(messages.get(position));
+                    if (view.getParent() == null) {
+                        addView(view);
+                    }
+                    isAnimStart = false;
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+                }
+            });
+        }
+    }
+
+    private TextView createTextView(T marqueeItem) {
+        TextView textView = (TextView) getChildAt((getDisplayedChild() + 1) % 3);
+        if (textView == null) {
+            textView = new TextView(getContext());
+            textView.setGravity(gravity | Gravity.CENTER_VERTICAL);
+            textView.setTextColor(textColor);
+            textView.setTextSize(textSize);
+            textView.setIncludeFontPadding(true);
+            textView.setSingleLine(singleLine);
+            if (singleLine) {
+                textView.setMaxLines(1);
+                textView.setEllipsize(TextUtils.TruncateAt.END);
+            }
+            if (typeface != null) {
+                textView.setTypeface(typeface);
+            }
+            textView.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (onItemClickListener != null) {
+                        onItemClickListener.onItemClick(getPosition(), (TextView) v);
+                    }
+                }
+            });
+        }
+        CharSequence message = "";
+        if (marqueeItem instanceof CharSequence) {
+            message = (CharSequence) marqueeItem;
+        } else if (marqueeItem instanceof IMarqueeItem) {
+            message = ((IMarqueeItem) marqueeItem).marqueeMessage();
+        }
+        textView.setText(message);
+        textView.setTag(position);
+        return textView;
+    }
 }
