8f9c20c0107547620a6584768d95147fd209a881
kolionsleep <kolionsleep@hotmail.com>
2021-10-29 09:00:01 +0800

    HpotoView

---
+++ app/src/main/java/bd/com/bdwallet/widget/photoview/PhotoView.java
@@ -0,0 +1,31 @@
+package bd.com.bdwallet.widget.photoview;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.PointF;
+import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.AttributeSet;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.Interpolator;
+import android.widget.ImageView;
+import android.widget.OverScroller;
+import android.widget.Scroller;
+
+
+public class PhotoView extends AppCompatImageView {
+
+    
+}
