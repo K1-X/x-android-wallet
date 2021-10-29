6f1343e471d891967ef6f1d3788a93e7202d281c
kolionsleep <kolionsleep@hotmail.com>
2021-10-29 10:10:01 +0800

    more define

--- app/src/main/java/bd/com/bdwallet/widget/photoview/PhotoView.java
+++ app/src/main/java/bd/com/bdwallet/widget/photoview/PhotoView.java
@@ -34,5 +34,72 @@ public class PhotoView extends AppCompatImageView {
     private int mMinRotate;
     private int mAnimaDuring;
     private float mMaxScale;
-    
+
+    private int MAX_OVER_SCROLL = 0;
+    private int MAX_FLING_OVER_SCROLL = 0;
+    private int MAX_OVER_RESISTANCE = 0;
+    private int MAX_ANIM_FROM_WAITE = 500;
+
+    private Matrix mBaseMatrix = new Matrix();
+    private Matrix mAnimaMatrix = new Matrix();
+    private Matrix mSynthesisMatrix = new Matrix();
+    private Matrix mTmpMatrix = new Matrix();
+
+    private RotateGestureDetector mRotateDetector;
+    private GestureDetector mDetector;
+    private ScaleGestureDetector mScaleDetector;
+    private OnClickListener mClickListener;
+
+    private ScaleType mScaleType;
+
+    private boolean hasMultiTouch;
+    private boolean hasDrawable;
+    private boolean isKnowSize;
+    private boolean hasOverTranslate;
+    private boolean isEnable = false;
+    private boolean isInit;
+    private boolean mAdjustViewBounds;
+    // 
+    private boolean isZoomUp;
+    private boolean canRotate;
+
+    private boolean imgLargeWidth;
+    private boolean imgLargeHeight;
+
+    private float mRotateFlag;
+    private float mDegrees;
+    private float mScale = 1.0f;
+    private int mTranslateX;
+    private int mTranslateY;
+
+    private float mHalfBaseRectWidth;
+    private float mHalfBaseRectHeight;
+
+    private RectF mWidgetRect = new RectF();
+    private RectF mBaseRect = new RectF();
+    private RectF mImgRect = new RectF();
+    private RectF mTmpRect = new RectF();
+    private RectF mCommonRect = new RectF();
+
+    private PointF mScreenCenter = new PointF();
+    private PointF mScaleCenter = new PointF();
+    private PointF mRotateCenter = new PointF();
+
+    private Transform mTranslate = new Transform();
+
+    private RectF mClip;
+    private Info mFromInfo;
+    private long mInfoTime;
+    private Runnable mCompleteCallBack;
+
+    private OnLongClickListener mLongClick;
+
+    //pull down transform like WeChat
+    boolean touchDown = false;
+    boolean canTransform = false;
+    float moveX = 0, moveY = 0;
+    float distanceX = 0, distanceY = 0;
+    float pullDownScale = 1F;
+    float bgAlpha = 1F;
+    long downTimestamp;    
 }
